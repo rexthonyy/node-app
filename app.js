b5c7ae46f03d71008e67c32bd936e21b41540406
rexthonyy <rexthonyy@gmail.com>
2022-03-31 15:33:04 +0100

    Some change

--- app.js
+++ app.js
@@ -1,65 +1,65 @@
 require("dotenv").config();
-const { makeQueryRunner } = require("./QueryRunner.js");
+// const { makeQueryRunner } = require("./QueryRunner.js");
 
-console.log(process.env.DATABASE_URL);
+// console.log(process.env.DATABASE_URL);
 
-async function main() {
-  const runner = await makeQueryRunner(
-    //"postgres:///graphile_cookbook",
-    process.env.DATABASE_URL,
-    "public"
-  );
+// async function main() {
+//   const runner = await makeQueryRunner(
+//     //"postgres:///graphile_cookbook",
+//     process.env.DATABASE_URL,
+//     "public"
+//   );
 
-  const result = await runner.query(
-    //"query PostsByAuthor($username: String!) { userByUsername(username: $username) { postsByAuthorId { nodes { id body topicByTopicId { id title } } } } }",
-    "query MyQuery {allUsers {nodes {name}}}"
-  );
+//   const result = await runner.query(
+//     //"query PostsByAuthor($username: String!) { userByUsername(username: $username) { postsByAuthorId { nodes { id body topicByTopicId { id title } } } } }",
+//     "query MyQuery {allUsers {nodes {name}}}"
+//   );
 
-  console.log(JSON.stringify(result, null, 2));
+//   console.log(JSON.stringify(result, null, 2));
 
-  await runner.release();
-}
+//   await runner.release();
+// }
 
-main().catch((e) => {
-  console.error(e);
-  process.exit(1);
+// main().catch((e) => {
+//   console.error(e);
+//   process.exit(1);
+// });
+
+
+
+
+
+
+
+
+
+
+const express = require('express');
+const { postgraphile } = require("postgraphile");
+const knowledgeBaseRouter = require('./api/apiKnowledgeBase');
+
+const app = express();
+
+app.use(
+    postgraphile(process.env.DATABASE_URL, {
+        watchPg: true,
+        graphiql: true,
+        enhanceGraphiql: true
+    })
+);
+
+app.use(express.json());
+    
+app.use('/knowledgebase', knowledgeBaseRouter);
+
+let port = process.env.PORT || 1000;
+var server = app.listen(port, function() {
+    console.log("Listening on port %s...", server.address().port);
 });
 
-
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const { postgraphile } = require("postgraphile");
-// const knowledgeBaseRouter = require('./api/apiKnowledgeBase');
-
-// const app = express();
-
-// app.use(
-//     postgraphile(process.env.DATABASE_URL, {
-//         watchPg: true,
-//         graphiql: true,
-//         enhanceGraphiql: true
-//     })
-// );
-
-// app.use(express.json());
-    
-// app.use('/knowledgebase', knowledgeBaseRouter);
-
-// let port = process.env.PORT || 1000;
-// var server = app.listen(port, function() {
-//     console.log("Listening on port %s...", server.address().port);
-// });
-
-// app.get("/", (req, res) => {
-//     res.json({ 
-//         status: "success", 
-//         message: "Welcome to the Knowledgebase app"
-//     });
-// });
+app.get("/", (req, res) => {
+    res.json({ 
+        status: "success", 
+        message: "Welcome to the Knowledgebase app"
+    });
+});
