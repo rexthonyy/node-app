89cb32b9850e2b00a06d79604a9899dc2ed243b7
rexthonyy <rexthonyy@gmail.com>
2022-04-01 13:43:22 +0100

    bug fix

--- schema/resolvers/getAllKnowledgeBaseResolver.js
+++ schema/resolvers/getAllKnowledgeBaseResolver.js
@@ -1,23 +1,35 @@
+const pgQueries = require('../../postgres/kb-queries');
+
+const init = () => {
+    return new Promise((resolve, reject) => {
+        
+        pgQueries.listKnowledgeBases(result => {
+            if(result.err){
+                return resolve(result.err);
+            }
+            resolve(result.res);
+        });
+    });
+}
+
 module.exports = () => {
-    // pgQueries.listKnowledgeBases(result => {
-    //     if(result.err){
-    //         return result.err;
-    //     }
-    //     return result.res;
-    // });
-    return {
-        id: 1,
-        name: "knowledgebase x",
-        icon: "icon",
-        footer: "some",
-        created_at: "382",
-        homepage_layout: "best",
-        category_layout: "chat",
-        active: true,
-        updated_at: "382",
-        front_page: "lss",
-        position: 21,
-        ui_color: "blue",
-        is_archived: false
-    };
+    init()
+    .then(result => {
+        return result;
+    });
+    // return {
+    //     id: 1,
+    //     name: "knowledgebase x",
+    //     icon: "icon",
+    //     footer: "some",
+    //     created_at: "382",
+    //     homepage_layout: "best",
+    //     category_layout: "chat",
+    //     active: true,
+    //     updated_at: "382",
+    //     front_page: "lss",
+    //     position: 21,
+    //     ui_color: "blue",
+    //     is_archived: false
+    // };
 }
