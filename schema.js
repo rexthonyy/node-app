0dde94394da07a21f250d8d2ba3bc61975fb5474
rexthonyy <rexthonyy@gmail.com>
2022-04-01 02:25:09 +0100

    Some change

--- schema.js
+++ schema.js
@@ -9,7 +9,7 @@ const {
     GraphQLBoolean
 } = require("graphql");
 
-const KnowledgeBaseType = GraphQLList({
+const KnowledgeBaseType = new GraphQLObjectType({
     name: "Knowledgebase",
     description: "App databases",
     fields: () => ({
