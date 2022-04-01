876e65c2fb9e3e9cf475a857c98c6a7cb8a959dd
rexthonyy <rexthonyy@gmail.com>
2022-04-01 18:02:17 +0100

    bug fix

--- schema/resolvers/createKnowledgebaseResolver.js
+++ schema/resolvers/createKnowledgebaseResolver.js
@@ -4,12 +4,12 @@ const util = require('../../util');
 const getData = ({name, icon, homepage_layout, category_layout, active, front_page, kb_locale_ids}) => {
     return new Promise((resolve, reject) => {
         
-        console.log(name);
-        console.log(icon);
-        console.log(homepage_layout);
-        console.log(category_layout);
-        console.log(active);
-        console.log(front_page);
+        console.log("name: " + name);
+        console.log("icon: " + icon);
+        console.log("homepage_layout: " + homepage_layout);
+        console.log("category_layout: " + category_layout);
+        console.log("active: " + active);
+        console.log("front_page: " + front_page);
         console.log(kb_locale_ids);
         resolve({status: "testing"});
     });
