27da3c5dbed666007b77b2af918d27009ddbd701
rexthonyy <rexthonyy@gmail.com>
2022-04-01 15:35:27 +0100

    bug fix

---
+++ schema/resolvers/getAllKnowledgeBaseTranslationStatusColorResolver.js
@@ -0,0 +1,181 @@
+const pgQueries = require('../../postgres/kb-queries');
+
+const getData = ({filter_by, knowledge_base_id, category_id}) => {
+    return new Promise((resolve, reject) => {
+        
+        if(!["article","category"].find(filterWith => filterWith == filter_by)) return reject({status:"error", message: "Please enter either 'article' or 'category' for filter_by"});
+
+        // get the category whose status we want to analyze
+        pgQueries.getKnowledgeBaseCategoriesByKnowledgeBaseIdAndParentId(knowledge_base_id, category_id, result => {
+            if(result.err){
+                result.err.errorIndex = 100;
+                return reject(result.err);
+            }
+
+
+            if(result.res.length == 0) {
+                result.res = [];
+            }
+
+            let categories = result.res;
+            
+            pgQueries.getKnowledgeBaseTranslationsByKnowledgeBaseId(knowledge_base_id, result => {
+                if(result.err){
+                    result.err.errorIndex = 200;
+                    return reject(result.err);
+                }
+
+                let kb_translations = result.res;
+                let num_translations = kb_translations.length;
+                let count = -1;
+
+                let kb_status_color_translations = [];
+
+                kb_translations.forEach(translation => {
+                    let num_categories = categories.length;
+                    let count1 = -1;
+
+                    let active_colors = [];
+
+                    categories.forEach(category => {
+                        if(filter_by == "category"){
+                            pgQueries.getKnowledgeBaseCategoryTranslationsByKnowledgeBaseIdCategoryIdAndLocaleId([knowledge_base_id, category.id, translation.kb_locale_id], result => {
+                                if(result.res == null || result.res.length==0) return checkComplete1();
+                                active_colors.push(result.res[0].ui_color);
+                                checkComplete1();
+                            });
+                        }else{
+                            pgQueries.getKnowledgeBaseArticleTranslationsByKnowledgeBaseIdCategoryIdAndLocaleId([knowledge_base_id, category.id, translation.kb_locale_id], result => {
+                                if(result.res == null || result.res.length==0) return checkComplete1();
+                                active_colors.push(result.res[0].ui_color);
+                                checkComplete1();
+                            });
+                        }
+                    });
+
+                    checkComplete1();
+                    function checkComplete1(){
+                        count1++;
+                        if(count1 == num_categories){
+                            //apply logic to get the main color for this translation
+                            let primary_color = "red";
+
+                            let numRed = 0;
+                            let numOrange = 0;
+                            let numGreen = 0;
+                            let numBlue = 0;
+                            let numGray = 0;
+                            let numYellow = 0;
+                            let numWhite = 0;
+
+                            active_colors.forEach(ui_color => {
+                                // logic goes here
+                                switch(ui_color){
+                                    case "red":
+                                        numRed++;
+                                    break;
+
+                                    case "orange":
+                                        numOrange++;
+                                    break;
+
+                                    case "green":
+                                        numGreen++;
+                                    break;
+
+                                    case "blue":
+                                        numBlue++;
+                                    break;
+
+                                    case "gray":
+                                        numGray++;
+                                    break;
+
+                                    case "yellow":
+                                        numYellow++;
+                                    break;
+
+                                    case "white":
+                                        numWhite++;
+                                    break;
+                                }
+                            });
+
+                            if(numOrange == num_categories){
+                                primary_color = "orange";
+                            }
+
+                            if(numGreen == num_categories){
+                                primary_color = "green";
+                            }
+
+                            if(numBlue == num_categories){
+                                primary_color = "blue";
+                            }
+
+                            if(numGray == num_categories){
+                                primary_color = "gray";
+                            }
+
+                            if(numRed == num_categories){
+                                primary_color = "red";
+                            }
+
+                            if(numGreen > 0){
+                                primary_color = "green";
+                            }
+
+                            if(numGray > 0){
+                                primary_color = "gray";
+                            }
+
+                            if(numBlue > 0){
+                                primary_color = "blue";
+                            }
+
+                            if(numYellow > 0){
+                                primary_color = "yellow";
+                            }
+
+                            if(numWhite > 0){
+                                primary_color = "white";
+                            }
+
+                            if(numOrange > 0){
+                                primary_color = "orange";
+                            }
+
+                            if(numRed > 0){
+                                primary_color = "red";
+                            }
+
+                            kb_status_color_translations.push({
+                                kb_locale_id: translation.kb_locale_id,
+                                knowledge_base_translation_id: translation.id,
+                                ui_color: primary_color,
+                                title: translation.title,
+                                default: translation.active
+                            });
+
+                            checkComplete();
+                        }
+                    }
+                });
+
+                checkComplete();
+
+                function checkComplete(){
+                    count++;
+                    if(count == num_translations){
+                        return resolve(kb_status_color_translations);
+                    }
+                }
+            });
+        });
+    });
+}
+
+module.exports = async (parents, args) => {
+    let result = await getData(args)
+    return result;
+}
