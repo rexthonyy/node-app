ab60893536981567b0fa6479252336a419284dfc
rexthonyy <rexthonyy@gmail.com>
2022-04-05 16:17:12 +0100

    get knowledge base category list

--- schema/resolvers/getKnowledgeBaseCategoriesResolver.js
+++ schema/resolvers/getKnowledgeBaseCategoriesResolver.js
@@ -17,49 +17,7 @@ const getData = ({ knowledge_base_id, kb_locale_id, level, parent_id }) => {
                 getCategoriesAtLevel(tree, level, index, level_categories => {
                     level_categories.sort(util.sortByPosition);
                     level_categories = filterOutArchive(level_categories);
-                    let response_data = [];
-
-                    level_categories.forEach(cat => {
-                        response_data.push({
-                            knowledge_base_category: {
-                                knowledge_base_category: {
-                                    id: cat.category_id,
-                                    knowledge_base_id: cat.knowledge_base_id,
-                                    parent_id: cat.parent_id,
-                                    position: cat.position,
-                                    created_at: cat.created_at,
-                                    updated_at: cat.updated_at,
-                                    is_archived: cat.is_archived,
-                                },
-                                knowledge_base_category_translation: {
-                                    id: cat.id,
-                                    name: cat.name,
-                                    kb_locale_id: cat.kb_locale_id,
-                                    category_id: cat.category_id,
-                                    created_at: cat.created_at,
-                                    updated_at: cat.updated_at,
-                                    ui_color: cat.ui_color,
-                                    category_icon: cat.category_icon,
-                                    title_tag: cat.title_tag,
-                                    footer: cat.footer,
-                                    keywords: cat.keywords,
-                                    meta_description: cat.meta_description,
-                                    publish_now: cat.publish_now,
-                                    active: cat.active,
-                                    permission: cat.permission,
-                                    update_metadata: cat.update_metadata,
-                                    is_delete_scheduled: cat.is_delete_scheduled,
-                                    is_update_scheduled: cat.is_update_scheduled,
-                                    knowledge_base_id: cat.knowledge_base_id,
-                                    is_archived: cat.is_archived,
-                                    list_id: cat.list_id
-                                }
-                            },
-                            stat: cat.stat
-                        });
-                    });
-
-                    resolve(response_data);
+                    resolveResponse(resolve, level_categories);
                 });
             });
         }else{
@@ -129,15 +87,60 @@ const getData = ({ knowledge_base_id, kb_locale_id, level, parent_id }) => {
                 function checkComplete(){
                     count++;
                     if(count == numCategories){
-                        console.log(kb_category_translations);
                         kb_category_translations.sort(util.sortByPosition);
                         kb_category_translations = filterOutArchive(kb_category_translations);
-                        resolve(kb_category_translations);
+                        resolveResponse(resolve, kb_category_translations);
                     }
                 }
             });
         }
     });
+}
+
+function resolveResponse(resolve, level_categories){
+    let response_data = [];
+
+    level_categories.forEach(cat => {
+        response_data.push({
+            knowledge_base_category: {
+                knowledge_base_category: {
+                    id: cat.category_id,
+                    knowledge_base_id: cat.knowledge_base_id,
+                    parent_id: cat.parent_id,
+                    position: cat.position,
+                    created_at: cat.created_at,
+                    updated_at: cat.updated_at,
+                    is_archived: cat.is_archived,
+                },
+                knowledge_base_category_translation: {
+                    id: cat.id,
+                    name: cat.name,
+                    kb_locale_id: cat.kb_locale_id,
+                    category_id: cat.category_id,
+                    created_at: cat.created_at,
+                    updated_at: cat.updated_at,
+                    ui_color: cat.ui_color,
+                    category_icon: cat.category_icon,
+                    title_tag: cat.title_tag,
+                    footer: cat.footer,
+                    keywords: cat.keywords,
+                    meta_description: cat.meta_description,
+                    publish_now: cat.publish_now,
+                    active: cat.active,
+                    permission: cat.permission,
+                    update_metadata: cat.update_metadata,
+                    is_delete_scheduled: cat.is_delete_scheduled,
+                    is_update_scheduled: cat.is_update_scheduled,
+                    knowledge_base_id: cat.knowledge_base_id,
+                    is_archived: cat.is_archived,
+                    list_id: cat.list_id
+                }
+            },
+            stat: cat.stat
+        });
+    });
+
+    resolve(response_data);
 }
 
 function filterOutArchive(obj){
