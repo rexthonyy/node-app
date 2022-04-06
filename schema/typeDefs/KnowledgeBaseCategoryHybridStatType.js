5465d473ae4fd5bdf6bdd3f0d668389bd2bca30d
rexthonyy <rexthonyy@gmail.com>
2022-04-06 12:28:29 +0100

    update knowledgebase category

--- schema/typeDefs/KnowledgeBaseCategoryHybridStatType.js
+++ schema/typeDefs/KnowledgeBaseCategoryHybridStatType.js
@@ -8,7 +8,7 @@ module.exports = new GraphQLObjectType({
     name: "KnowledgeBaseCategoryHybridStatType",
     description: "This returns an object of the knowledge base category, knowledge base category translation and stat type",
     fields: () => ({
-        knowledge_base_category: { type: KnowledgeBaseCategoryHybridType},
+        data: { type: KnowledgeBaseCategoryHybridType},
         stat: { type: KnowledgeBaseCategoryStatType}
     })
 });
