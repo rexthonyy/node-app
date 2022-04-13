2b48aa6b653d270b4c14a20132dd0884530fc8f6
rexthony <rexthonyy@gmail.com>
2022-04-13 17:17:27 +0100

    tickets

--- tickets/schema/typeDefs/RootQueryType.js
+++ tickets/schema/typeDefs/RootQueryType.js
@@ -6,6 +6,13 @@ const {
     GraphQLList
 } = require("graphql");
 
+
+// data types
+const TicketArticleType = require("./TicketArticleType");
+
+// resolvers
+const getTicketArticlesByTicketIdResolver = require("../resolvers/getTicketArticlesByTicketIdResolver");
+
 module.exports = new GraphQLObjectType({
     name: "Query",
     description: 'Root Query',
@@ -13,6 +20,14 @@ module.exports = new GraphQLObjectType({
         ping: {
             type: GraphQLString,
             resolve: () => "pong"
+        },
+        getTicketArticlesByTicketId_: {
+            type: GraphQLList(TicketArticleType),
+            description: "Returns a list of ticket articles",
+            args: {
+                ticket_id: { type: GraphQLNonNull(GraphQLID) }
+            },
+            resolve: getTicketArticlesByTicketIdResolver
         }
     })
 });
