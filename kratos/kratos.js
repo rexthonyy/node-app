7ad6850612aaa0f66ded6a15f9887ba11a993771
rexthony <rexthonyy@gmail.com>
2022-04-22 16:27:53 +0100

    update

--- kratos/kratos.js
+++ kratos/kratos.js
@@ -6,10 +6,9 @@ require('./postgres/initialize_dbs').init()
   const { ApolloServer } = require("apollo-server");
   const { graphqlHTTP } = require('express-graphql');
   const { createGraphQLSchema } = require("openapi-to-graphql");
-  //const oas1 = require("./api.openapi.json");
-  //const oas2 = require("./openapi.json");
-  const oas = require("./openapi1.json");
-  const { schema, report1 } = await createGraphQLSchema([oas]);
+  const oas1 = require("./api.openapi.json");
+  const oas2 = require("./openapi1.json");
+  const { schema, report1 } = await createGraphQLSchema([oas1, oas2]);
   const schema1 = require('./schema/index');
 
   const app = express();
