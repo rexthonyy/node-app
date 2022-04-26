e4db8b74a31668782f7425e0da70e75c9726ac66
rexthony <rexthonyy@gmail.com>
2022-04-26 15:10:12 +0100

    update

--- kratos/schema/typeDefs/RootQueryType.js
+++ kratos/schema/typeDefs/RootQueryType.js
@@ -8,9 +8,11 @@ const {
 
 // data types
 const ErrorContainer = require("./ErrorContainer");
+const LoginFlow = require("./LoginFlow");
 
 // resolvers
 const getErrorContainerResolver = require("../resolvers/getErrorContainerResolver");
+const getSelfServiceLoginFlowResolver = require("../resolvers/getSelfServiceLoginFlowResolver");
 
 module.exports = new GraphQLObjectType({
     name: "Query",
@@ -22,17 +24,19 @@ module.exports = new GraphQLObjectType({
         },
         errorContainer_: {
             type: ErrorContainer,
-            description: `
-            This endpoint returns the error associated with a user-facing self service errors.
-
-            This endpoint supports stub values to help you implement the error UI:
-
-            ?error=stub:500 - returns a stub 500 (Internal Server Error) error.
-            `,
+            description: "This endpoint returns the error associated with a user-facing self service errors. \nThis endpoint supports stub values to help you implement the error UI: ?error=stub:500 - returns a stub 500 (Internal Server Error) error.",
             args: {
                 error: { type: GraphQLNonNull(GraphQLString) }
             },
             resolve: getErrorContainerResolver
+        },
+        getSelfServiceLoginFlow_: {
+            type: LoginFlow,
+            description: "This endpoint returns a login flow's context with, for example, error details and other information.",
+            args: {
+                id: { type: GraphQLNonNull(GraphQLString) }
+            },
+            resolve: getSelfServiceLoginFlowResolver
         },
     })
 });
