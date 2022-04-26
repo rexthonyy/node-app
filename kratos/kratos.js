1acee506ad3eb4e1419f85ab34109afa9de656a0
rexthony <rexthonyy@gmail.com>
2022-04-26 14:19:13 +0100

    update

--- kratos/kratos.js
+++ kratos/kratos.js
@@ -12,7 +12,6 @@ require('./postgres/initialize_dbs').init()
   // const oas1 = require("./openapi1.json");
   // const { schema, report1 } = await createGraphQLSchema([oas1]);
   const schema1 = require('./schema/index');
-  console.log(schema);
 
   const app = express();
 
--- kratos/schema/resolvers/getErrorContainerResolver.js
+++ kratos/schema/resolvers/getErrorContainerResolver.js
@@ -3,12 +3,12 @@ const pgKratosQueries = require('../../postgres/kratos-queries');
 const getData = ({error}) => {
     return new Promise((resolve, reject) => {
         resolve({
-            errors: {
+            errors: JSON.stringify({
                 "code": 500,
                 "message": error,
                 "reason": "some reason",
                 "debug": "some debug info"
-            },
+            }),
             id: "9f900efa-a5ea-4dfd-8311-a8c7448ffeec"
         });
     });
