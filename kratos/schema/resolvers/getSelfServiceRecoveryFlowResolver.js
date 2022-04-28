e47ac872ce9eb037a984aef04f1d31014f0c2391
rexthony <rexthonyy@gmail.com>
2022-04-28 17:27:04 +0100

    update

--- kratos/schema/resolvers/getSelfServiceRecoveryFlowResolver.js
+++ kratos/schema/resolvers/getSelfServiceRecoveryFlowResolver.js
@@ -2,37 +2,39 @@ const pgKratosQueries = require('../../postgres/kratos-queries');
 
 const getData = ({id}) => {
     console.log(id);
-    pgKratosQueries.getSelfServiceRecoveryFlowById([id], result => {
-        if(result.err){
-            return reject("Recovery flow ID not found");
-        }
+    return new Promise((resolve, reject) => {
+        pgKratosQueries.getSelfServiceRecoveryFlowById([id], result => {
+            if(result.err){
+                return reject("Recovery flow ID not found");
+            }
 
-        let selfServiceRecoveryFlow = result.res[0];
+            let selfServiceRecoveryFlow = result.res[0];
 
-        let active = selfServiceRecoveryFlow.active_method;
-        let expiresAt = selfServiceRecoveryFlow.expires_at;
-        let issuedAt = selfServiceRecoveryFlow.issued_at;
-        let requestUrl = selfServiceRecoveryFlow.request_url;
-        let messages = [{
-            context: "api",
-            id: 1,
-            text: "update",
-            type: "container"
-        }];
-        let methods = "{}";
-        let state = selfServiceRecoveryFlow.state;
-        let type = selfServiceRecoveryFlow.type;
+            let active = selfServiceRecoveryFlow.active_method;
+            let expiresAt = selfServiceRecoveryFlow.expires_at;
+            let issuedAt = selfServiceRecoveryFlow.issued_at;
+            let requestUrl = selfServiceRecoveryFlow.request_url;
+            let messages = [{
+                context: "api",
+                id: 1,
+                text: "update",
+                type: "container"
+            }];
+            let methods = "{}";
+            let state = selfServiceRecoveryFlow.state;
+            let type = selfServiceRecoveryFlow.type;
 
-        resolve({
-            active,
-            expiresAt,
-            id,
-            issuedAt,
-            messages,
-            methods,
-            requestUrl,
-            state,
-            type
+            resolve({
+                active,
+                expiresAt,
+                id,
+                issuedAt,
+                messages,
+                methods,
+                requestUrl,
+                state,
+                type
+            });
         });
     });
 }
