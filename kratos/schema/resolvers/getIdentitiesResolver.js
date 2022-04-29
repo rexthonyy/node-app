b3fb4e67a44c7b2f6ed045f31e36712949f71987
rexthony <rexthonyy@gmail.com>
2022-04-29 14:24:01 +0100

    update

--- kratos/schema/resolvers/getIdentitiesResolver.js
+++ kratos/schema/resolvers/getIdentitiesResolver.js
@@ -1,8 +1,8 @@
 const pgKratosQueries = require('../../postgres/kratos-queries');
 
 const getData = ({page, perPage}) => {
-    if(page == null || page == 0) page = null;
-    if(perPage == null) perPage = 0;
+    //if(page == null || page == 0) page = null;
+    //if(perPage == null) perPage = 0;
 
     return new Promise((resolve, reject) => {
         pgKratosQueries.getIdentities([page, perPage], result => {
