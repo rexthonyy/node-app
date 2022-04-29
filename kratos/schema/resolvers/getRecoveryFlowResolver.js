324a2c0c3c47a68da9fc5ad6c507c33ff4abe8de
rexthony <rexthonyy@gmail.com>
2022-04-29 18:02:40 +0100

    update

--- kratos/schema/resolvers/getRecoveryFlowResolver.js
+++ kratos/schema/resolvers/getRecoveryFlowResolver.js
@@ -1,7 +1,42 @@
 const pgKratosQueries = require('../../postgres/kratos-queries');
-
+const util = require('../../libs/util');
 const getData = () => {
     return new Promise((resolve, reject) => {
+        let id = util.getSessionId();
+        pgKratosQueries.getSelfServiceRecoveryFlowById([id], result => {
+            if(result.err || result.res.length == 0){
+                return reject("Recovery flow ID not found");
+            }
+
+            let selfServiceRecoveryFlow = result.res[0];
+
+            let active = selfServiceRecoveryFlow.active_method;
+            let expiresAt = selfServiceRecoveryFlow.expires_at;
+            let issuedAt = selfServiceRecoveryFlow.issued_at;
+            let requestUrl = selfServiceRecoveryFlow.request_url;
+            let messages = [{
+                context: "api",
+                id: 1,
+                text: "update",
+                type: "container"
+            }];
+            let methods = "{}";
+            let state = selfServiceRecoveryFlow.state;
+            let type = selfServiceRecoveryFlow.type;
+
+            resolve({
+                active,
+                expiresAt,
+                id,
+                issuedAt,
+                messages,
+                methods,
+                requestUrl,
+                state,
+                type
+            });
+        });
+        /*
         resolve({
             active: "active",
             expiresAt: "2022-01",
@@ -18,6 +53,7 @@ const getData = () => {
             state: "active",
             type: "browser"
         });
+        */
     });
 }
 
