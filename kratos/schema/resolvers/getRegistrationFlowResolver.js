f1cbd2534320e73804481c369e0afdf9916dd28e
rexthony <rexthonyy@gmail.com>
2022-04-30 13:05:21 +0100

    update

--- kratos/schema/resolvers/getRegistrationFlowResolver.js
+++ kratos/schema/resolvers/getRegistrationFlowResolver.js
@@ -2,6 +2,39 @@ const pgKratosQueries = require('../../postgres/kratos-queries');
 
 const getData = () => {
     return new Promise((resolve, reject) => {
+        let id = util.getSessionId();
+        pgKratosQueries.getSelfServiceRegistrationFlowById([id], result => {
+            if(result.err || result.res.length == 0){
+                return reject("Registration flow ID not found");
+            }
+
+            let selfServiceRecoveryFlow = result.res[0];
+
+            let active = selfServiceRecoveryFlow.active_method;
+            let expiresAt = selfServiceRecoveryFlow.expires_at;
+            let issuedAt = selfServiceRecoveryFlow.issued_at;
+            let requestUrl = selfServiceRecoveryFlow.request_url;
+            let messages = [{
+                context: "api",
+                id: 1,
+                text: "update",
+                type: "container"
+            }];
+            let methods = "{}";
+            let type = selfServiceRecoveryFlow.type;
+
+            resolve({
+                active,
+                expiresAt,
+                id,
+                issuedAt,
+                messages,
+                methods,
+                requestUrl,
+                type
+            });
+        });
+        /*
         resolve({
             active: "active",
             expiresAt: "2022-01",
@@ -16,7 +49,7 @@ const getData = () => {
             methods: "post",
             requestUrl: "/root",
             type: "browser"
-        });
+        });*/
     });
 }
 
