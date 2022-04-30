c4cdeaef163ca2ed634cd14d8113652c7f272770
rexthony <rexthonyy@gmail.com>
2022-04-30 13:26:20 +0100

    update

--- kratos/schema/resolvers/getVerificationFlowResolver.js
+++ kratos/schema/resolvers/getVerificationFlowResolver.js
@@ -1,8 +1,42 @@
 const pgKratosQueries = require('../../postgres/kratos-queries');
-
+const util = require('../../libs/util');
 const getData = () => {
     return new Promise((resolve, reject) => {
-        resolve({
+        let id = util.getSessionId();
+        pgKratosQueries.getSelfServiceVerificationFlowById([id], result => {
+            if(result.err || result.res.length == 0){
+                return reject("Verification flow ID not found");
+            }
+
+            let selfServiceVerificationFlow = result.res[0];
+
+            let active = selfServiceVerificationFlow.active_method;
+            let expiresAt = selfServiceVerificationFlow.expires_at;
+            let issuedAt = selfServiceVerificationFlow.issued_at;
+            let requestUrl = selfServiceVerificationFlow.request_url;
+            let messages = [{
+                context: "api",
+                id: 1,
+                text: "update",
+                type: "container"
+            }];
+            let methods = "{}";
+            let state = selfServiceVerificationFlow.state;
+            let type = selfServiceVerificationFlow.type;
+
+            resolve({
+                active,
+                expiresAt,
+                id,
+                issuedAt,
+                messages,
+                methods,
+                requestUrl,
+                state,
+                type
+            });
+        });
+        /*resolve({
             active: "active",
             expiresAt: "2022-01",
             id: "id2",
@@ -17,7 +51,7 @@ const getData = () => {
             requestUrl: "/root",
             state: "active",
             type: "browser"
-        });
+        });*/
     });
 }
 
