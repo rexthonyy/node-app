0f11331ba8b041ec847b9b7123982347294aba3a
rexthony <rexthonyy@gmail.com>
2022-04-30 15:00:00 +0100

    update

--- kratos/schema/resolvers/getSessionResolver.js
+++ kratos/schema/resolvers/getSessionResolver.js
@@ -24,26 +24,66 @@ const getData = ({authorization, cookie}) => {
             let authenticatedAt = session.authenticated_at;
             let expiresAt = session.expires_at;
             let id = session.id;
-            let identity = session.identity_id;
             let issuedAt = session.issued_at;
 
-            resolve({
-                active,
-                authenticatedAt,
-                expiresAt,
-                id,
-                identity,
-                issuedAt
-            })
+            pgKratosQueries.getIdentityById([session.identity_id], result => {
+                if(result.err || result.res.length == 0){
+                    return reject("Identity not found");
+                }
+    
+                let identity = result.res[0];
+    
+                let identityType = {
+                    id: identity.id,
+                    schemaId: identity.schema_id,
+                    schemaUrl: "",
+                    traits: JSON.stringify(identity.traits),
+                    recoveryAddresses: [],
+                    verifiableAddresses: []
+                };
+    
+                pgKratosQueries.getRecoveryAddressesByIdentityId([identity.id], result => {
+                    if(result.err){
+                        result.res = [];
+                    }
+                    let recoveryAddresses = result.res;
+                    recoveryAddresses.forEach(recoveryAddress => {
+                        identityType.recoveryAddresses.push({
+                            id: recoveryAddress.id,
+                            value: recoveryAddress.value,
+                            via: recoveryAddress.via
+                        });
+                    });
+    
+                    pgKratosQueries.getVerifiableAddressesByIdentityId([identity.id], result => {
+                        if(result.err){
+                            result.res = [];
+                        }
+                        let verifiableAddresses = result.res;
+                        verifiableAddresses.forEach(verifiableAddress => {
+                            identityType.verifiableAddresses.push({
+                                id: verifiableAddress.id,
+                                status: verifiableAddress.status,
+                                value: verifiableAddress.value,
+                                verified: verifiableAddress.verified,
+                                verifiedAt: verifiableAddress.verified_at,
+                                via: verifiableAddress.via
+                            });
+                        });
+    
+                        resolve({
+                            active,
+                            authenticatedAt,
+                            expiresAt,
+                            id,
+                            identityType,
+                            issuedAt
+                        });
+                    });
+                });
+            });
+
         });
-        /*resolve({
-            active: false,
-            authenticatedAt: "2022-01",
-            expiresAt: "2022-01",
-            id: 1,
-            identity: "32453",
-            issuedAt: "2022-02"
-        });*/
     });
 }
 
