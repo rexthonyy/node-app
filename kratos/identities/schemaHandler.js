d664478b89dca850455f015ce06866be8cd4245e
rexthony <rexthonyy@gmail.com>
2022-05-03 13:51:10 +0100

    update

--- kratos/identities/schemaHandler.js
+++ kratos/identities/schemaHandler.js
@@ -1,34 +1,15 @@
 const getJSONSchemaById = (id) => {
-    return `
-    {
-        "$id": "ory://identity-test-schema",
-        "$schema": "http://json-schema.org/draft-07/schema#",
-        "title": "IdentitySchema",
-        "type": "object",
-        "properties": {
-          "traits": {
-            "type": "object",
-            "properties": {
-              "name": {
-                "type": "object",
-                "properties": {
-                  "first": {
-                    "type": "string"
-                  },
-                  "last": {
-                    "type": "string"
-                  }
-                }
-              }
-            },
-            "required": [
-              "name"
-            ],
-            "additionalProperties": true
-          }
-        }
-      }
-    `;
+  return {
+    "id": "/SimpleAddress",
+    "type": "object",
+    "properties": {
+      "firstName": {"type": "string"},
+      "lastName": {"type": "string"},
+      "email": {"type": "string"},
+      "password": {"type": "string"}
+    },
+    "required": ["firstName","lastName","email","password"]
+  };
 };
 
 const getDefaultJSONSchema = (id) => {
--- kratos/schema/resolvers/executeCreateIdentityResolver.js
+++ kratos/schema/resolvers/executeCreateIdentityResolver.js
@@ -1,8 +1,50 @@
+var Validator = require('jsonschema').Validator;
+var v = new Validator();
+const {uuid} = require('uuidv4');
 const pgKratosQueries = require('../../postgres/kratos-queries');
+const schemaHandler = require('../../identities/schemaHandler');
+const getIdentityById = require('../resolverUtils/getIdentityById');
 
 const getData = ({createIdentityInput}) => {
     return new Promise((resolve, reject) => {
-        resolve({
+        let schemaId = createIdentityInput.schemaId;
+        let traits = JSON.parse(createIdentityInput.traits);
+
+        let identitySchema = schemaHandler.getJSONSchemaById(schemaId);
+        let errors = v.validate(traits,identitySchema).errors
+        if(errors.length > 0){
+            return reject(JSON.stringify(errors));
+        }
+
+        let now = new Date().toUTCString();
+        let values = [
+            uuid(),
+            schemaId,
+            JSON.stringify(traits),
+            now,
+            now,
+            NETWORK_ID,
+            "active",
+            now
+        ];
+        pgKratosQueries.createIdentity(values, result => {
+            if(result.err){
+                console.error(result.err);
+                return reject("Identity could not be created");
+            }
+
+            let identityId = result.res.id;
+
+            getIdentityById(identityId, identity => {
+                if(typeof identity == "string"){
+                    return reject(identity);
+                }
+
+                resolve(identity);
+            });
+        });
+
+        /*resolve({
             id: "202232",
             recoveryAddresses: [{ 
                 id: "202232",
@@ -20,7 +62,7 @@ const getData = ({createIdentityInput}) => {
                 verifiedAt: "202232",
                 via: "api",
             }]
-        });
+        });*/
     });
 }
 
