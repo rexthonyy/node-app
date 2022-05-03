cb2b68c7fa68ff2ea2578618dc703ea658dbc1f3
rexthony <rexthonyy@gmail.com>
2022-05-03 17:08:35 +0100

    update

--- kratos/schema/resolvers/executeCompleteSelfServiceSettingsFlowWithProfileMethodResolver.js
+++ kratos/schema/resolvers/executeCompleteSelfServiceSettingsFlowWithProfileMethodResolver.js
@@ -1,8 +1,81 @@
+var Validator = require('jsonschema').Validator;
+var v = new Validator();
 const pgKratosQueries = require('../../postgres/kratos-queries');
+const getIdentityById = require('../resolverUtils/getIdentityById');
+const schemaHandler = require('../../identities/schemaHandler');
 
 const getData = ({flow, selfServiceSettingsMethodsProfileInput}) => {
     return new Promise((resolve, reject) => {
-        resolve({
+        let traits = JSON.parse(selfServiceSettingsMethodsProfileInput);
+
+        pgKratosQueries.getSelfServiceSettingsFlowById([flow], result => {
+            if(result.err || result.res.length == 0){
+                return reject("Settings flow ID not found");
+            }
+
+            let selfServiceSettingsFlow = result.res[0];
+
+            let id = selfServiceSettingsFlow.id;
+            let active = selfServiceSettingsFlow.active_method;
+            let expiresAt = selfServiceSettingsFlow.expires_at;
+            let issuedAt = selfServiceSettingsFlow.issued_at;
+            let requestUrl = selfServiceSettingsFlow.request_url;
+            let identityId = selfServiceSettingsFlow.identity_id;
+            let messages = selfServiceSettingsFlow.ui.messages;
+            let methods = selfServiceSettingsFlow.ui.method;
+            let state = selfServiceSettingsFlow.state;
+            let type = selfServiceSettingsFlow.type;
+
+            getIdentityById(identityId, identity => {
+                if(typeof identity == "string"){
+                    return reject(identity);
+                }
+                
+                const settingsFlow = {
+                    active,
+                    expiresAt,
+                    id,
+                    identity,
+                    issuedAt,
+                    messages,
+                    methods,
+                    requestUrl,
+                    state,
+                    type
+                };
+
+                if(expiresAt != null){
+                    let expireDate = new Date(expiresAt);
+                    if(Date.now() > expireDate.getTime()){
+                        return reject("Settings Flow ID expired");
+                    }
+                }
+
+                let schemaId = identity.schemaId;
+                let identitySchema = schemaHandler.getJSONSchemaById(schemaId);
+                let errors = v.validate(traits,identitySchema).errors
+                if(errors.length > 0){
+                    return reject(JSON.stringify(errors));
+                }
+
+                let now = new Date().toUTCString();
+                let values = [
+                    identityId,
+                    schemaId,
+                    traits,
+                    now
+                ];
+                pgKratosQueries.updateIdentity(values, result => {
+                    if(result.err || result.res.length == 0){
+                        return reject("Identity could not be updated");
+                    }
+
+                    resolve(settingsFlow);
+                });
+            });
+        });
+
+        /*resolve({
             active: "active",
             expiresAt: "2022-01",
             id: "id-1",
@@ -18,7 +91,7 @@ const getData = ({flow, selfServiceSettingsMethodsProfileInput}) => {
             requestUrl: "/root",
             state: "active",
             type: "browser"
-        });
+        });*/
     });
 }
 
