6fa77cc8b31ea683b08eafb684d3bc53ea609d1a
rexthony <rexthonyy@gmail.com>
2022-05-07 16:07:38 +0100

    update

--- spicedb/spice.js
+++ spicedb/spice.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 require('./postgres/initialize_dbs').init()
-.then(() => {
+.then(async () => {
   const pg = require("pg");
   const express = require('express');
   const { stitchSchemas } = require('@graphql-tools/stitch');
