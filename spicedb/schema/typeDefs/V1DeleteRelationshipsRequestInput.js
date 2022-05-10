4a143bd2baabd8bf7552552d6a2587af0d2825ca
rexthony <rexthonyy@gmail.com>
2022-05-10 18:07:42 +0100

    update

--- spicedb/schema/typeDefs/V1DeleteRelationshipsRequestInput.js
+++ spicedb/schema/typeDefs/V1DeleteRelationshipsRequestInput.js
@@ -11,7 +11,7 @@ module.exports = new GraphQLInputObjectType({
     name: "V1DeleteRelationshipsRequestInput_",
     description: "DeleteRelationshipsRequest specifies which Relationships should be deleted, requesting the delete of ALL relationships that match the specified filters. If the optional_preconditions parameter is included, all of the specified preconditions must also be satisfied before the delete will be executed.",
     fields: () => ({
-        optionalPrecondition: { type: GraphQLNonNull(V1PreconditionInput) },
+        optionalPrecondition: { type: V1PreconditionInput },
         relationshipFilter: { type: V1RelationshipFilterInput, description: "RelationshipFilter is a collection of filters which when applied to a relationship will return relationships that have exactly matching fields.\n\nresource_type is required. All other fields are optional and if left unspecified will not filter relationships." }
     })
 });
