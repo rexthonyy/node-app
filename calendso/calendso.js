c7716c8518a419e0e26e92246c2c0b31acee6652
rexthony <rexthonyy@gmail.com>
2022-05-13 13:09:39 +0100

    update

---
+++ calendso/calendso.js
@@ -0,0 +1,64 @@
+require("dotenv").config();
+require('./postgres/initialize_dbs').init()
+.then(() => {
+  const express = require('express');
+  const { stitchSchemas } = require('@graphql-tools/stitch');
+  const { ApolloServer } = require("apollo-server");
+  const { createGraphQLSchema } = require("openapi-to-graphql");
+  const { graphqlHTTP } = require('express-graphql');
+  const oas = require("./calendso.json");
+  const { schema } = await createGraphQLSchema([oas]);
+  const schema1 = require('./schema/index');
+
+  const app = express();
+
+  app.use(express.static('public'));
+  app.use(express.json());
+  
+  async function main() {
+    const server = new ApolloServer({
+      schema: stitchSchemas({
+      subschemas: [
+        {
+          schema: schema
+        },
+        {
+          schema: schema1
+        }
+      ]
+      }),
+      plugins: [plugin],
+      uploads: false
+    });
+  
+    const { url } = await server.listen({port: process.env.PORT1});
+    console.log(`🚀 Server ready at ${url}`);
+
+
+    app.use('/graphql',
+    graphqlHTTP({
+        schema: stitchSchemas({
+      subschemas: [
+        {
+          schema: schema
+        },
+        {
+          schema: schema1
+        }
+      ]
+      }),
+        graphiql: true,
+    }));
+    
+    let port = process.env.PORT || 1000;
+    var lesServer = app.listen(port, function() {
+        console.log("Listening on port %s...", lesServer.address().port);
+    });
+  }
+
+  main().catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
+
+});
