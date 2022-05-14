24e295c741ed3883fb94d7a96ec0223356e25516
rexthony <rexthonyy@gmail.com>
2022-05-14 18:07:46 +0100

    update

--- dolibarr/dolibarr.js
+++ dolibarr/dolibarr.js
@@ -1,32 +1,32 @@
 require("dotenv").config();
 require('./postgres/initialize_dbs').init()
-.then(() => {
-  const pg = require("pg");
+.then(async () => {
   const express = require('express');
   const { stitchSchemas } = require('@graphql-tools/stitch');
   const { ApolloServer } = require("apollo-server");
-  const { makeSchemaAndPlugin } = require("postgraphile-apollo-server");
+  const oas = require("./openapi.json");
+  const { schema } = await createGraphQLSchema([oas]);
   const { graphqlHTTP } = require('express-graphql');
   const schema1 = require('./schema/index');
 
   const app = express();
 
-  const pgPool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL
-  });
+  // const pgPool = new pg.Pool({
+  //   connectionString: process.env.DATABASE_URL
+  // });
   
   app.use(express.static('public'));
   app.use(express.json());
   
   async function main() {
-    const { schema, plugin } = await makeSchemaAndPlugin(
-      pgPool,
-      'public', // PostgreSQL schema to use
-      {
-        // PostGraphile options, see:
-        // https://www.graphile.org/postgraphile/usage-library/
-      }
-    );
+    // const { schema, plugin } = await makeSchemaAndPlugin(
+    //   pgPool,
+    //   'public', // PostgreSQL schema to use
+    //   {
+    //     // PostGraphile options, see:
+    //     // https://www.graphile.org/postgraphile/usage-library/
+    //   }
+    // );
 
     const server = new ApolloServer({
       schema: stitchSchemas({
