9a47d522357c40afda2f28edd93357f855903ef9
rexthony <rexthonyy@gmail.com>
2022-05-19 13:33:06 +0100

    update

--- services/app.js
+++ services/app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const { ApolloServer } = require("apollo-server");
 const { graphqlHTTP } = require('express-graphql');
-//const schema1 = require('./schema/index');
+const schema1 = require('./schema/index');
 
 const app = express();
 
@@ -17,11 +17,11 @@ app.use(express.json());
 // console.log(`🚀 Server ready at ${url}`);
 
 
-// app.use('/graphql', 
-// graphqlHTTP({
-//     schema: schema1,
-//     graphiql: true,
-// }));
+app.use('/graphql', 
+graphqlHTTP({
+    schema: schema1,
+    graphiql: true,
+}));
 
 let port = process.env.PORT || 1000;
 var lesServer = app.listen(port, function() {
