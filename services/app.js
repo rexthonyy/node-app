f91b73fda50f04947d5badcf452d35a1b5541915
rexthony <rexthonyy@gmail.com>
2022-05-19 13:32:15 +0100

    update

--- services/app.js
+++ services/app.js
@@ -2,36 +2,28 @@ require("dotenv").config();
 const express = require('express');
 const { ApolloServer } = require("apollo-server");
 const { graphqlHTTP } = require('express-graphql');
-const schema1 = require('./schema/index');
+//const schema1 = require('./schema/index');
 
 const app = express();
 
-app.use(express.static('public'));
 app.use(express.json());
 
-async function main() {
-  const server = new ApolloServer({
-    schema: schema1,
-    uploads: false
-  });
+// const server = new ApolloServer({
+//   schema: schema1,
+//   uploads: false
+// });
 
-  const { url } = await server.listen({port: process.env.PORT1});
-  console.log(`🚀 Server ready at ${url}`);
+// const { url } = await server.listen({port: process.env.PORT1});
+// console.log(`🚀 Server ready at ${url}`);
 
 
-  app.use('/graphql', 
-  graphqlHTTP({
-      schema: schema1,
-      graphiql: true,
-  }));
-  
-  let port = process.env.PORT || 1000;
-  var lesServer = app.listen(port, function() {
-      console.log("Listening on port %s...", lesServer.address().port);
-  });
-}
+// app.use('/graphql', 
+// graphqlHTTP({
+//     schema: schema1,
+//     graphiql: true,
+// }));
 
-main().catch(e => {
-  console.error(e);
-  process.exit(1);
+let port = process.env.PORT || 1000;
+var lesServer = app.listen(port, function() {
+    console.log("Listening on port %s...", lesServer.address().port);
 });
