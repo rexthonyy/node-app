337906397a6a1c31426eb01e701254178166b947
rexthony <rexthonyy@gmail.com>
2022-05-31 13:59:04 +0100

    update

--- rolesAndPermissions/schema/index.js
+++ rolesAndPermissions/schema/index.js
@@ -3,7 +3,7 @@ const { loadSchemaSync } = require('@graphql-tools/load');
 const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader');
 const { addResolversToSchema } = require('@graphql-tools/schema');
 const resolvers = require("./resolvers");
-console.log(__dirname);
+
 const schema = loadSchemaSync(join(__dirname, './schema.graphql'), {
     loaders: [new GraphQLFileLoader()]
 });
