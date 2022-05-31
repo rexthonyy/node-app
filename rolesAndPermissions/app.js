9df92b81927aa67f764694a7caf3ffe95b46b38f
rexthony <rexthonyy@gmail.com>
2022-05-31 13:58:04 +0100

    update

--- rolesAndPermissions/app.js
+++ rolesAndPermissions/app.js
@@ -3,6 +3,7 @@ require('./postgres/initialize_dbs').init()
     .then(async() => {
         const Sentry = require("@sentry/node");
         Sentry.init({ dsn: "http://d78601a2198e422d8855c8be53f57061@88.208.212.249:8000/2" });
+        const express = require('express');
         const { graphqlHTTP } = require('express-graphql');
         const schema = require('./schema');
 
