3ced20a10b336717f680d37e614e68062633d6f6
rexthony <rexthonyy@gmail.com>
2022-05-31 15:36:20 +0100

    update

--- rolesAndPermissions/permissions/index.js
+++ rolesAndPermissions/permissions/index.js
@@ -56,63 +56,63 @@ if ("Noob".cannot('delete resources')) {
 }
 
 // basic group checks
-if ("Marak".isIn('Administrators')) {
-    sys.puts('Marak is in Administrators');
-}
+// if ("Marak".isIn('Administrators')) {
+//     sys.puts('Marak is in Administrators');
+// }
 
-if (!"Marak".isntIn('Administrators')) {
-    sys.puts('Marak is in Administrators');
-}
+// if (!"Marak".isntIn('Administrators')) {
+//     sys.puts('Marak is in Administrators');
+// }
 
-if (!"Noob".isIn('Administrators')) {
-    sys.puts('Noob is not in Administrators');
-}
+// if (!"Noob".isIn('Administrators')) {
+//     sys.puts('Noob is not in Administrators');
+// }
 
-if ("Noob".isntIn('Administrators')) {
-    sys.puts('Noob is not in Administrators');
-}
-
-
-
-// add a user
-roles.addUser('Bob');
-
-// add a role
-roles.addRole('is allowed to go fly fishing');
-
-// add group
-roles.addGroup('Fly Fishers');
-
-// add user to group
-roles.addUserToGroup('Bob', 'Fly Fishers');
-
-// add user to group
-roles.addRoleToGroup('is allowed to go fly fishing', 'Fly Fishers');
-
-// inherit roles from group, in this case we give "Fly Fishers" all the same roles as "Guests"
-//roles.inheritRolesFromGroup('Fly Fishers', 'Guests');
-
-
-// now that we have loaded some roles and created some new ones, lets try some test cases
-
-// first, lets see all the current groups and their roles
-var theGroups = roles.getGroups();
-//sys.puts(JSON.stringify(theGroups, true, 1));
-// okay that was interesting, but kinda hard to read. lets try a basic permissions check
+// if ("Noob".isntIn('Administrators')) {
+//     sys.puts('Noob is not in Administrators');
+// }
 
 
 
-var someuser = "Bob";
-if (!someuser.inGroup("Administrators")) {
-    sys.puts(someuser + ' is not in the "Administrators" group.')
-}
+// // add a user
+// roles.addUser('Bob');
 
-if (someuser.can('edit a resource')) {
-    // i don't think so Bob!
-} else {
-    sys.puts('"Bob" cannot "edit a resource" because he is not part of any group that has this role.')
-}
+// // add a role
+// roles.addRole('is allowed to go fly fishing');
 
-if ("Alexis".can("add a resource")) {
-    sys.puts('"Alexis" can "add a resource" since he is the "Moderators" group');
-}
+// // add group
+// roles.addGroup('Fly Fishers');
+
+// // add user to group
+// roles.addUserToGroup('Bob', 'Fly Fishers');
+
+// // add user to group
+// roles.addRoleToGroup('is allowed to go fly fishing', 'Fly Fishers');
+
+// // inherit roles from group, in this case we give "Fly Fishers" all the same roles as "Guests"
+// //roles.inheritRolesFromGroup('Fly Fishers', 'Guests');
+
+
+// // now that we have loaded some roles and created some new ones, lets try some test cases
+
+// // first, lets see all the current groups and their roles
+// var theGroups = roles.getGroups();
+// //sys.puts(JSON.stringify(theGroups, true, 1));
+// // okay that was interesting, but kinda hard to read. lets try a basic permissions check
+
+
+
+// var someuser = "Bob";
+// if (!someuser.inGroup("Administrators")) {
+//     sys.puts(someuser + ' is not in the "Administrators" group.')
+// }
+
+// if (someuser.can('edit a resource')) {
+//     // i don't think so Bob!
+// } else {
+//     sys.puts('"Bob" cannot "edit a resource" because he is not part of any group that has this role.')
+// }
+
+// if ("Alexis".can("add a resource")) {
+//     sys.puts('"Alexis" can "add a resource" since he is the "Moderators" group');
+// }
