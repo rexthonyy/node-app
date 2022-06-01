4a6526b427abdfe382f6acb5c2b29f3c7e093820
rexthony <rexthonyy@gmail.com>
2022-06-01 14:03:34 +0100

    update

--- rolesAndPermissions/schema/resolvers/lib/index.js
+++ rolesAndPermissions/schema/resolvers/lib/index.js
@@ -1,23 +1,17 @@
-const roles = require('../../../permissions');
+require('../../../permissions');
 
 const hasPermission = (permissions, required) => {
     let found = false;
     for (let p in permissions) {
-        console.log(p);
-        if (p.isIn("Permissions")) {
-            for (let i = 0; i < required.length; i++) {
-                let r = required[i];
-                console.log(r);
-                if (p == r) {
-                    if (permissions[p] == true) {
-                        found = true;
-                        break;
-                    }
+        for (const r of required) {
+            if (p == r) {
+                if (permissions[p] == true) {
+                    found = true;
+                    break;
                 }
             }
         }
     }
-    console.log(found);
     return found;
 }
 
