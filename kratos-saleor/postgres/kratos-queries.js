16860920c449c4c5a93d783f34d57dead422dd20
rexthony <rexthonyy@gmail.com>
2022-06-01 11:12:23 +0100

    update

--- kratos-saleor/postgres/kratos-queries.js
+++ kratos-saleor/postgres/kratos-queries.js
@@ -163,180 +163,8 @@ const createAccountUserAddress = (values, response) => {
     });
 };
 
-const createRecoveryFlow = (values, response) => {
-    client.query('INSERT INTO selfservice_recovery_flows (id, request_url, issued_at, expires_at, active_method, csrf_token, state, recovered_identity_id, created_at, updated_at, type, ui, nid) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *', values, (err, res) => {
-        if (err) {
-            response({
-                err: err.stack,
-                res: null,
-                code: 219
-            });
-        } else {
-            response({
-                err: null,
-                res: res.rows[0]
-            });
-        }
-    });
-};
-
-const createRegistrationFlow = (values, response) => {
-    client.query('INSERT INTO selfservice_registration_flows (id, request_url, issued_at, expires_at, active_method, csrf_token, created_at, updated_at, type, ui, nid, internal_context) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *', values, (err, res) => {
-        if (err) {
-            response({
-                err: err.stack,
-                res: null,
-                tes: 220
-            });
-        } else {
-            response({
-                err: null,
-                res: res.rows[0]
-            });
-        }
-    });
-};
-
-const createSettingsFlow = (values, response) => {
-    client.query('INSERT INTO selfservice_settings_flows (id, request_url, issued_at, expires_at, identity_id, created_at, updated_at, active_method, state, type, ui, nid, internal_context) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *', values, (err, res) => {
-        if (err) {
-            response({
-                err: err.stack,
-                res: null,
-                tes: 221
-            });
-        } else {
-            response({
-                err: null,
-                res: res.rows[0]
-            });
-        }
-    });
-};
-
-const createVerificationFlow = (values, response) => {
-    client.query('INSERT INTO selfservice_verification_flows (id, request_url, issued_at, expires_at, csrf_token, created_at, updated_at, type, state, active_method, ui, nid) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *', values, (err, res) => {
-        if (err) {
-            response({
-                err: err.stack,
-                res: null,
-                tes: 222
-            });
-        } else {
-            response({
-                err: null,
-                res: res.rows[0]
-            });
-        }
-    });
-};
-
-const createSession = (values, response) => {
-    client.query('INSERT INTO sessions (id, issued_at, expires_at, authenticated_at, identity_id, created_at, updated_at, token, active, nid, logout_token, aal, authentication_methods) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *', values, (err, res) => {
-        if (err) {
-            response({
-                err: err.stack,
-                res: null,
-                tes: 223
-            });
-        } else {
-            response({
-                err: null,
-                res: res.rows[0]
-            });
-        }
-    });
-};
-
-const createIdentity = (values, response) => {
-    client.query('INSERT INTO identities (id, schema_id, traits, created_at, updated_at, nid, state, state_changed_at) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *', values, (err, res) => {
-        if (err) {
-            response({
-                err: err.stack,
-                res: null,
-                tes: 224
-            });
-        } else {
-            response({
-                err: null,
-                res: res.rows[0]
-            });
-        }
-    });
-};
-
-const createIdentityCredentials = (values, response) => {
-    client.query('INSERT INTO identity_credentials (id, config, identity_credential_type_id, identity_id, created_at, updated_at, nid) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *', values, (err, res) => {
-        if (err) {
-            response({
-                err: err.stack,
-                res: null,
-                tes: 225
-            });
-        } else {
-            response({
-                err: null,
-                res: res.rows[0]
-            });
-        }
-    });
-};
-
-const createIdentityCredentialIdentifier = (values, response) => {
-    client.query('INSERT INTO identity_credential_identifiers (id, identifier, identity_credential_id, created_at, updated_at, nid, identity_credential_type_id) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *', values, (err, res) => {
-        if (err) {
-            response({
-                err: err.stack,
-                res: null,
-                tes: 226
-            });
-        } else {
-            response({
-                err: null,
-                res: res.rows[0]
-            });
-        }
-    });
-};
-
-const createIdentityRecoveryAddress = (values, response) => {
-    client.query('INSERT INTO identity_recovery_addresses (id, via, value, identity_id, created_at, updated_at, nid) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *', values, (err, res) => {
-        if (err) {
-            response({
-                err: err.stack,
-                res: null,
-                tes: 227
-            });
-        } else {
-            response({
-                err: null,
-                res: res.rows[0]
-            });
-        }
-    });
-};
-
-const createIdentityRecoveryToken = (values, response) => {
-    client.query('INSERT INTO identity_recovery_tokens (id, token, used, used_at, identity_recovery_address_id, selfservice_recovery_flow_id, created_at, updated_at, expires_at, issued_at, nid) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *', values, (err, res) => {
-        if (err) {
-            response({
-                err: err.stack,
-                res: null,
-                tes: 228
-            });
-        } else {
-            response({
-                err: null,
-                res: res.rows[0]
-            });
-        }
-    });
-};
-
-
-
-const updateIdentity = (values, response) => {
-    client.query('UPDATE identities SET schema_id=$2, traits=$3, updated_at=$4 WHERE id=$1 RETURNING *', values, (err, res) => {
+const updateAccountUserActive = (values, response) => {
+    client.query(`UPDATE ${db.account_user} SET is_active=$2 WHERE id=$1 RETURNING *`, values, (err, res) => {
         if (err) {
             response({
                 err: err.stack,
@@ -346,63 +174,12 @@ const updateIdentity = (values, response) => {
         } else {
             response({
                 err: null,
-                res: res.rows[0]
-            });
-        }
-    });
-};
-
-const updateIdentityCredentials = (values, response) => {
-    client.query('UPDATE identity_credentials SET config=$2, updated_at=$3 WHERE id=$1 RETURNING *', values, (err, res) => {
-        if (err) {
-            response({
-                err: err.stack,
-                res: null,
-                test: 230
-            });
-        } else {
-            response({
-                err: null,
-                res: res.rows[0]
-            });
-        }
-    });
-};
-
-const createSessionToken = (values, response) => {
-    client.query(`INSERT INTO ${db.oauth_sessions} (token) VALUES($1) RETURNING *`, values, (err, res) => {
-        if (err) {
-            response({
-                err: err.stack,
-                res: null,
-                tes: 227
-            });
-        } else {
-            response({
-                err: null,
-                res: res.rows[0]
-            });
-        }
-    });
-};
-
-
-const getSessionToken = (values, response) => {
-    pool.query(`SELECT * from ${db.oauth_sessions} WHERE token=$1`, values, (err, res) => {
-        if (err) {
-            response({
-                err: err,
-                res: null,
-                code: 216
-            });
-        } else {
-            response({
-                err: null,
                 res: res.rows
             });
         }
     });
 };
+
 module.exports = {
     getUserByEmail,
     getUserById,
@@ -414,20 +191,5 @@ module.exports = {
     createAccountAddress,
     createAccountUserAddress,
 
-    createRecoveryFlow,
-    createRegistrationFlow,
-    createSettingsFlow,
-    createVerificationFlow,
-    createSession,
-    createIdentity,
-    createIdentityCredentials,
-    createIdentityCredentialIdentifier,
-    createIdentityRecoveryAddress,
-    createIdentityRecoveryToken,
-
-    updateIdentity,
-    updateIdentityCredentials,
-
-    createSessionToken,
-    getSessionToken,
+    updateAccountUserActive,
 }
--- kratos-saleor/schema/resolvers/confirmAccount.js
+++ kratos-saleor/schema/resolvers/confirmAccount.js
@@ -1,19 +1,35 @@
 const jwt = require('jsonwebtoken');
 const { isEmailValid } = require("../../libs/util");
+const pgKratosQueries = require("../../postgres/kratos-queries");
+const { getGraphQLUserById } = require("./lib");
 
 module.exports = async(parent, args, context) => {
     return new Promise((resolve, reject) => {
         let email = args.email;
         let token = args.token;
 
-        console.log(email);
-        console.log(token);
-
         if (!isEmailValid(email)) return resolve(getError("email", "Email format not supported", "INVALID_CREDENTIALS", null));
 
         jwt.verify(token, process.env.AUTHORIZATION_TOKEN_SECRET, (err, payload) => {
             if (err) return resolve(getError("token", "Email format not supported", "JWT_INVALID_TOKEN", null));
-            console.log(payload);
+            let user_id = payload.sub;
+            pgKratosQueries.getUserById([user_id], result => {
+                if (result.err) return resolve(getError("token", "Email format not supported", "JWT_INVALID_TOKEN", null));
+                let accountUser = result.res[0];
+                jwt.verify(payload.data, accountUser.jwt_token_key, (err, user) => {
+                    if (result.err) return resolve(getError("token", "Email format not supported", "JWT_DECODE_ERROR", user));
+                    if (!(user.id == user_id && user.email == email)) return resolve(getError("token", "Email format not supported", "JWT_DECODE_ERROR", user));
+                    let values = [
+                        user_id,
+                        true
+                    ];
+                    pgKratosQueries.updateAccountUserActive(values, async result => {
+                        if (result.err) return resolve(getError("failed to update user", result.err, "GRAPHQL_ERROR", user));
+                        let graphQLUser = await getGraphQLUserById(accountUser.id);
+                        resolve(getResult(graphQLUser));
+                    });
+                });
+            });
         });
     });
 }
@@ -32,6 +48,15 @@ function getError(field, message, code, addressType, user) {
             code,
             addressType
         }],
+        user
+    };
+}
+
+
+function getResult(user) {
+    return {
+        errors: [{}],
+        accountErrors: [{}],
         user
     };
 }
