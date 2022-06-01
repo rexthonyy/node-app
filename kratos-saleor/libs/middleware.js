a4d1af094959a62bd1709d766c593e521706b482
rexthony <rexthonyy@gmail.com>
2022-06-01 13:32:11 +0100

    update

--- kratos-saleor/libs/middleware.js
+++ kratos-saleor/libs/middleware.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken');
-const pgKratosQueries = require('../postgres/kratos-queries');
 const { getGraphQLUserById } = require('../schema/resolvers/lib');
 
 module.exports = async(resolve, root, args, context, info) => {
@@ -11,17 +10,14 @@ let getAuthenticatedUser = (context) => {
     return new Promise((resolve) => {
         const authToken = context.headers["authorization-bearer"];
         if (!authToken) return resolve(null);
-        jwt.verify(authToken, process.env.ACCESS_TOKEN_SECRET, (err, authUser) => {
+        jwt.verify(authToken, process.env.ACCESS_TOKEN_SECRET, async(err, authUser) => {
             if (err || authUser == null) return resolve(null);
-            pgKratosQueries.getUserById([authUser.id], async result => {
-                if (result.err || result.res.length == 0) return cb(null);
-                try {
-                    let graphQLUser = await getGraphQLUserById(user.id);
-                    resolve(graphQLUser);
-                } catch (err) {
-                    resolve(null);
-                }
-            });
+            try {
+                let graphQLUser = await getGraphQLUserById(Number(authUser.sub));
+                resolve(graphQLUser);
+            } catch (err) {
+                resolve(null);
+            }
         });
     });
 }
