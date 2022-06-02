de1c7e33a5010d4b47d25e62623399d8d60559df
rexthony <rexthonyy@gmail.com>
2022-06-02 12:15:36 +0100

    update

--- rolesAndPermissions/schema/resolvers/lib/index.js
+++ rolesAndPermissions/schema/resolvers/lib/index.js
@@ -1,20 +1,22 @@
 require('../../../permissions');
 
-const hasPermission = (permissions, required) => {
-    let found = false;
+const hasAllPermissions = (permissions, required) => {
+    let found = [];
+
     for (let p in permissions) {
         for (const r of required) {
             if (p == r) {
                 if (permissions[p] == true) {
-                    found = true;
+                    found.push(true);
                     break;
                 }
             }
         }
     }
-    return found;
+
+    return found.length == required.length;
 }
 
 module.exports = {
-    hasPermission
+    hasAllPermissions
 }
--- rolesAndPermissions/schema/resolvers/permissionGroupCreate.js
+++ rolesAndPermissions/schema/resolvers/permissionGroupCreate.js
@@ -1,5 +1,5 @@
 // Create new permission group. Requires one of the following permissions: MANAGE_STAFF.
-const { hasPermission } = require('./lib');
+const { hasAllPermissions } = require('./lib');
 const permissionsdbQueries = require('../../postgres/permissionsdb-queries');
 const getIdentityById = require('./lib/getIdentityById');
 
@@ -9,7 +9,7 @@ module.exports = async(parent, args, context) => {
         let users = args.input.addUsers;
         let groupName = args.input.name;
 
-        if (hasPermission(context.variables, ["PERMISSION_MANAGE_STAFF"])) {
+        if (hasAllPermissions(context.variables, ["PERMISSION_MANAGE_STAFF"])) {
 
             for (let i = 0, j = permissions.length; i < j; i++) {
                 permissions[i] = permissions[i].toLowerCase();
