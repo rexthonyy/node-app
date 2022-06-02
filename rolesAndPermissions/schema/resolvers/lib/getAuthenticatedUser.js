dc6e0f68e1309c6ab0c6d51576d9a8c2f27d6b45
rexthony <rexthonyy@gmail.com>
2022-06-02 14:55:19 +0100

    update

--- rolesAndPermissions/schema/resolvers/lib/getAuthenticatedUser.js
+++ rolesAndPermissions/schema/resolvers/lib/getAuthenticatedUser.js
@@ -1,8 +1,27 @@
 const pgKratosQueries = require('../../../postgres/kratos-queries');
+const getIdentityById = require('./getIdentityById');
 
 let getAuthenticatedUser = (context, cb) => {
-    console.log(context);
-    cb(null);
+    const token = context.header["token"];
+    pgKratosQueries.getSessionByToken([token], result => {
+        if (result.err || result.res.length == 0) {
+            return cb(null);
+        }
+
+        let session = result.res[0];
+
+        if (!session.active) {
+            return cb(null);
+        }
+
+        getIdentityById(session.identity_id, identity => {
+            if (typeof identity == "string") {
+                return cb(null);
+            } else {
+                cb(identity.traits);
+            }
+        });
+    });
 }
 
 module.exports = getAuthenticatedUser;
