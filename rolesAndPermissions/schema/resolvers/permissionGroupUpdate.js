2918f0baee62caa8fe24f6cd0885649eecc1d4ee
rexthony <rexthonyy@gmail.com>
2022-06-03 13:21:16 +0100

    update

--- rolesAndPermissions/schema/resolvers/permissionGroupUpdate.js
+++ rolesAndPermissions/schema/resolvers/permissionGroupUpdate.js
@@ -22,14 +22,7 @@ module.exports = async(parent, args, context) => {
             let removeUsers = args.input.removeUsers;
 
             if (hasAllPermissions(context.body.variables, ["PERMISSION_MANAGE_STAFF"])) {
-                for (let i = 0, j = permissions.length; i < j; i++) {
-                    permissions[i] = permissions[i].toLowerCase();
-                }
-                for (let i = 0, j = removePermissions.length; i < j; i++) {
-                    removePermissions[i] = removePermissions[i].toLowerCase();
-                }
-
-                permissionsdbQueries.updateAuthGroupById([groupId, groupName], result => {
+                permissionsdbQueries.getAuthGroupById([groupId], result => {
                     if (result.err) {
                         return resolve(getError(
                             "name",
@@ -40,116 +33,28 @@ module.exports = async(parent, args, context) => {
                         ));
                     }
 
-                    getAuthGroupPermissionsByGroupId(groupId, authGroupPermissions => {
-                        let newGroupPermissions = [];
-                        for (let k = 0, l = permissions.length; k < l; k++) {
-                            let isFound = false;
-                            for (let i = 0, j = authGroupPermissions.length; i < j; i++) {
-                                if (permissions[k] == authGroupPermissions[i].code.toLowerCase()) {
-                                    isFound = true;
-                                    break;
-                                }
-                            }
-                            if (!isFound) {
-                                newGroupPermissions.push(permissions[k]);
-                            }
-                        }
+                    if (result.res.length == 0) {
+                        return resolve(getError(
+                            "name",
+                            "Permission Group not found",
+                            null, [],
+                            users,
+                            null
+                        ));
+                    }
 
-                        const numNewGroupPermissions = newGroupPermissions.length;
-                        let countNewGroupPermissions = -1;
+                    let authGroup = result.res[0];
 
-                        newGroupPermissions.forEach(newGroupPermissionCodename => {
-                            permissionsdbQueries.getAuthPermissionsByCodename("codename=$1", [newGroupPermissionCodename], result => {
-                                if (result.res && result.res.length > 0) {
-                                    let authPermission = result.res[0];
-                                    permissionsdbQueries.createAuthGroupPermission([groupId, authPermission.id], result => {
-                                        checkNewGroupPermissionComplete();
-                                    });
-                                } else {
-                                    checkNewGroupPermissionComplete();
-                                }
-                            });
-                        });
-
-                        checkNewGroupPermissionComplete();
-
-                        function checkNewGroupPermissionComplete() {
-                            countNewGroupPermissions++;
-                            if (countNewGroupPermissions == numNewGroupPermissions) {
-
-                                permissionsdbQueries.getAccountUserGroupsByGroupId([groupId], result => {
-                                    if (result.err || result.res.length == 0) {
-                                        return cb([]);
-                                    }
-
-                                    let newGroupUsers = [];
-                                    let accountUserGroupRows = result.res;
-
-                                    for (let k = 0, l = users.length; k < l; k++) {
-                                        let isFound = false;
-                                        for (let i = 0, j = accountUserGroupRows.length; i < j; i++) {
-                                            if (users[k] == accountUserGroupRows[i].user_id) {
-                                                isFound = true;
-                                                break;
-                                            }
-                                        }
-                                        if (!isFound) {
-                                            newGroupUsers.push(users[k]);
-                                        }
-                                    }
-
-                                    const numNewGroupUsers = newGroupUsers.length;
-                                    let countNewGroupUsers = -1;
-
-                                    newGroupUsers.forEach(newGroupUser => {
-                                        permissionsdbQueries.createAccountUserGroup([newGroupUser, groupId], result => {
-                                            checkNewGroupUsersComplete();
-                                        });
-                                    });
-
-                                    checkNewGroupUsersComplete();
-
-                                    function checkNewGroupUsersComplete() {
-                                        countNewGroupUsers++;
-                                        if (countNewGroupUsers == numNewGroupUsers) {
-                                            let idsOfPermissionsToRemove = [];
-                                            for (let k = 0, l = removePermissions.length; k < l; k++) {
-                                                for (let i = 0, j = authGroupPermissions.length; i < j; i++) {
-                                                    if (removePermissions[k] == authGroupPermissions[i].code.toLowerCase()) {
-                                                        idsOfPermissionsToRemove.push(authGroupPermissions[i].id);
-                                                        break;
-                                                    }
-                                                }
-                                            }
-
-                                            const numIdsOfPermissionsToRemove = idsOfPermissionsToRemove.length;
-                                            let countIdsOfPermissionsToRemove = -1;
-
-                                            idsOfPermissionsToRemove.forEach(groupPermissionId => {
-                                                permissionsdbQueries.deleteAuthGroupPermissionsByPermissionsId([groupId, groupPermissionId], result => {
-                                                    checkIdsOfPermissionsToRemoveComplete();
-                                                });
-                                            });
-
-                                            checkIdsOfPermissionsToRemoveComplete();
-
-                                            function checkIdsOfPermissionsToRemoveComplete() {
-                                                countIdsOfPermissionsToRemove++;
-                                                if (countIdsOfPermissionsToRemove == numIdsOfPermissionsToRemove) {
-                                                    const numUsers = removeUsers.length;
-                                                    let countUsersToRemove = -1;
-
-                                                    removeUsers.forEach(userId => {
-                                                        permissionsdbQueries.deleteAccountUserGroupsByUserId([userId, groupId], result => {
-                                                            checkUsersToRemoveComplete();
-                                                        });
-                                                    });
-
-                                                    checkUsersToRemoveComplete();
-
-                                                    function checkUsersToRemoveComplete() {
-                                                        countUsersToRemove++;
-                                                        if (countUsersToRemove == numUsers) {
+                    updateGroupName(groupId, groupName)
+                        .then(() => {
+                            addGroupPermissions(groupId, permissions)
+                                .then(() => {
+                                    addNewUsersToGroupPermissions(groupId, users)
+                                        .then(() => {
+                                            removeGroupPermissions(groupId, removePermissions)
+                                                .then(() => {
+                                                    removeGroupUsers(groupId, removeUsers)
+                                                        .then(() => {
                                                             permissionsdbQueries.getAccountUserGroupsByGroupId([groupId], result => {
                                                                 let usersInGroup = [];
                                                                 if (!(result.err || result.res.length == 0)) {
@@ -160,19 +65,14 @@ module.exports = async(parent, args, context) => {
                                                                 }
 
                                                                 updateUserPermission(authUser, usersInGroup, () => {
-                                                                    getGroups(resolve, authUser, groupId, groupName);
+                                                                    getGroups(resolve, authUser, authGroup);
                                                                 });
                                                             });
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
+                                                        });
+                                                });
+                                        });
                                 });
-                            }
-                        }
-                    });
+                        });
                 });
             }
         });
@@ -192,6 +92,184 @@ function getError(field, message, code, permissions, users, group) {
         group
     };
 }
+
+function updateGroupName(groupId, groupName) {
+    return new Promise((resolve) => {
+        if (!groupName) return resolve();
+        permissionsdbQueries.updateAuthGroupById([groupId, groupName], result => {
+            if (result.err) {
+                return resolve(getError(
+                    "name",
+                    result.err,
+                    null, [],
+                    users,
+                    null
+                ));
+            }
+
+            resolve();
+        });
+    });
+}
+
+function addGroupPermissions(groupId, permissions) {
+    return new Promise((resolve) => {
+        if (!permissions) return resolve();
+        getAuthGroupPermissionsByGroupId(groupId, authGroupPermissions => {
+            for (let i = 0, j = permissions.length; i < j; i++) {
+                permissions[i] = permissions[i].toLowerCase();
+            }
+
+            let newGroupPermissions = [];
+            for (let k = 0, l = permissions.length; k < l; k++) {
+                let isFound = false;
+                for (let i = 0, j = authGroupPermissions.length; i < j; i++) {
+                    if (permissions[k] == authGroupPermissions[i].code.toLowerCase()) {
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (!isFound) {
+                    newGroupPermissions.push(permissions[k]);
+                }
+            }
+
+            const numNewGroupPermissions = newGroupPermissions.length;
+            let countNewGroupPermissions = -1;
+
+            newGroupPermissions.forEach(newGroupPermissionCodename => {
+                permissionsdbQueries.getAuthPermissionsByCodename("codename=$1", [newGroupPermissionCodename], result => {
+                    if (result.res && result.res.length > 0) {
+                        let authPermission = result.res[0];
+                        permissionsdbQueries.createAuthGroupPermission([groupId, authPermission.id], result => {
+                            checkNewGroupPermissionComplete();
+                        });
+                    } else {
+                        checkNewGroupPermissionComplete();
+                    }
+                });
+            });
+
+            checkNewGroupPermissionComplete();
+
+            function checkNewGroupPermissionComplete() {
+                countNewGroupPermissions++;
+                if (countNewGroupPermissions == numNewGroupPermissions) {
+                    resolve();
+                }
+            }
+        });
+    });
+}
+
+function addNewUsersToGroupPermissions(groupId, users) {
+    return new Promise((resolve) => {
+        if (!users) return resolve();
+        permissionsdbQueries.getAccountUserGroupsByGroupId([groupId], result => {
+            if (result.err || result.res.length == 0) {
+                return resolve();
+            }
+
+            let newGroupUsers = [];
+            let accountUserGroupRows = result.res;
+
+            for (let k = 0, l = users.length; k < l; k++) {
+                let isFound = false;
+                for (let i = 0, j = accountUserGroupRows.length; i < j; i++) {
+                    if (users[k] == accountUserGroupRows[i].user_id) {
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (!isFound) {
+                    newGroupUsers.push(users[k]);
+                }
+            }
+
+            const numNewGroupUsers = newGroupUsers.length;
+            let countNewGroupUsers = -1;
+
+            newGroupUsers.forEach(newGroupUser => {
+                permissionsdbQueries.createAccountUserGroup([newGroupUser, groupId], result => {
+                    checkNewGroupUsersComplete();
+                });
+            });
+
+            checkNewGroupUsersComplete();
+
+            function checkNewGroupUsersComplete() {
+                countNewGroupUsers++;
+                if (countNewGroupUsers == numNewGroupUsers) {
+                    resolve();
+                }
+            }
+        });
+    });
+}
+
+function removeGroupPermissions(groupId, removePermissions) {
+    return new Promise((resolve) => {
+        if (!removePermissions) return resolve();
+        getAuthGroupPermissionsByGroupId(groupId, authGroupPermissions => {
+            for (let i = 0, j = removePermissions.length; i < j; i++) {
+                removePermissions[i] = removePermissions[i].toLowerCase();
+            }
+
+            let idsOfPermissionsToRemove = [];
+            for (let k = 0, l = removePermissions.length; k < l; k++) {
+                for (let i = 0, j = authGroupPermissions.length; i < j; i++) {
+                    if (removePermissions[k] == authGroupPermissions[i].code.toLowerCase()) {
+                        idsOfPermissionsToRemove.push(authGroupPermissions[i].id);
+                        break;
+                    }
+                }
+            }
+
+            const numIdsOfPermissionsToRemove = idsOfPermissionsToRemove.length;
+            let countIdsOfPermissionsToRemove = -1;
+
+            idsOfPermissionsToRemove.forEach(groupPermissionId => {
+                permissionsdbQueries.deleteAuthGroupPermissionsByPermissionsId([groupId, groupPermissionId], result => {
+                    checkIdsOfPermissionsToRemoveComplete();
+                });
+            });
+
+            checkIdsOfPermissionsToRemoveComplete();
+
+            function checkIdsOfPermissionsToRemoveComplete() {
+                countIdsOfPermissionsToRemove++;
+                if (countIdsOfPermissionsToRemove == numIdsOfPermissionsToRemove) {
+                    resolve();
+                }
+            }
+        });
+    });
+}
+
+function removeGroupUsers(groupId, removeUsers) {
+    return new Promise((resolve) => {
+        if (!removeUsers) return resolve();
+        const numUsers = removeUsers.length;
+        let countUsersToRemove = -1;
+
+        removeUsers.forEach(userId => {
+            permissionsdbQueries.deleteAccountUserGroupsByUserId([userId, groupId], result => {
+                checkUsersToRemoveComplete();
+            });
+        });
+
+        checkUsersToRemoveComplete();
+
+        function checkUsersToRemoveComplete() {
+            countUsersToRemove++;
+            if (countUsersToRemove == numUsers) {
+                resolve();
+            }
+        }
+    });
+}
+
+
 
 function updateUserPermission(authUser, users, cb) {
     const numUsers = users.length;
@@ -233,10 +311,9 @@ function updateUserPermission(authUser, users, cb) {
     }
 }
 
-
-function getGroups(resolve, authUser, groupId, groupName) {
-    getAuthGroupPermissionsByGroupId(groupId, permissions => {
-        getUsersInGroupId(groupId, users => {
+function getGroups(resolve, authUser, group) {
+    getAuthGroupPermissionsByGroupId(group.id, permissions => {
+        getUsersInGroupId(group.id, users => {
             let authUserPermissions = authUser ? (authUser.userPermissions ? authUser.userPermissions : []) : [];
             let userCanManage = false;
 
@@ -250,21 +327,21 @@ function getGroups(resolve, authUser, groupId, groupName) {
             let res = {
                 errors: [{
                     field: null,
-                    message: "Group updated successfully",
+                    message: "Group created successfully",
                     code: "REQUIRED",
                     permissions: null,
                     users: null
                 }],
                 group: {
-                    id: groupId,
-                    name: groupName,
+                    id: group.id,
+                    name: group.name,
                     users,
                     permissions,
                     userCanManage
                 },
                 permissionGroupErrors: [{
                     field: null,
-                    message: "Group updated successfully",
+                    message: "Group created successfully",
                     code: "REQUIRED",
                     permissions: null,
                     users: null
