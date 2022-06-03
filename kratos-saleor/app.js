2341f59fabd6699f8fa702c1bf64fa84c3a9e9bd
rexthony <rexthonyy@gmail.com>
2022-06-03 16:54:13 +0100

    update

--- kratos-saleor/app.js
+++ kratos-saleor/app.js
@@ -11,6 +11,7 @@ require('./postgres/initialize_dbs').init()
         //const schema1 = require('./schema/index');
         const { loadSchemaSync } = require('@graphql-tools/load');
         const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader');
+        const utils = require('./libs/util');
         const schema = loadSchemaSync("schema.graphql", {
             loaders: [new GraphQLFileLoader()]
         });
@@ -42,6 +43,38 @@ require('./postgres/initialize_dbs').init()
         let port = process.env.PORT || 1000;
         var lesServer = app.listen(port, function() {
             console.log("Listening on port %s...", lesServer.address().port);
+        });
+
+        app.get('/login', utils.isAuthenticatd, (req, res) => {
+            let callbackUrl = req.query.callbackUrl;
+            if (req.kratosSession != null) {
+
+            }
+            res.render('login');
+        });
+
+        app.post('/login', utils.isAuthenticated, (req, res) => {
+            let email = req.body.email;
+            let password = req.body.password;
+
+            console.log(email);
+            console.log(password);
+        });
+
+        app.get('/signup', utils.isAuthenticatd, (req, res) => {
+            let callbackUrl = req.query.callbackUrl;
+            if (req.kratosSession != null) {
+
+            }
+            res.render('signup');
+        });
+
+        app.post('/signup', utils.isAuthenticated, (req, res) => {
+            let email = req.body.email;
+            let password = req.body.password;
+
+            console.log(email);
+            console.log(password);
         });
 
         app.use(Sentry.Handlers.errorHandler());
--- kratos-saleor/libs/util.js
+++ kratos-saleor/libs/util.js
@@ -1,11 +1,27 @@
-function isEmailValid(email){
-	return String(email)
-		.toLowerCase()
-		.match(
-		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-		);
+const jwt = require('jsonwebtoken');
+const pgQueries = require('../postgres/kratos-queries');
+
+function isEmailValid(email) {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+}
+
+function isAuthenticated(req, res, next) {
+    const accessToken = req.cookies[consts.COOKIE_ID];
+    if (accessToken == undefined) return next();
+    pgQueries.getSessionByToken([accessToken], result => {
+        if (result.err || result.res.length == 0) return next();
+        let session = result.res[0];
+        if (!session.active) return next();
+        req.kratosSession = session;
+        next();
+    });
 }
 
 module.exports = {
-    isEmailValid
+    isEmailValid,
+    isAuthenticated
 }
