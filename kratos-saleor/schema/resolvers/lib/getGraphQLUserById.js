f5ac928ff9475c8aeb333b6e0859477d35d1399d
rexthony <rexthonyy@gmail.com>
2022-06-08 17:01:31 +0100

    update

--- kratos-saleor/schema/resolvers/lib/getGraphQLUserById.js
+++ kratos-saleor/schema/resolvers/lib/getGraphQLUserById.js
@@ -10,8 +10,8 @@ let getGraphQLUserById = (user_id) => {
 
             let accountUser = result.res[0];
             let addresses = await getAccountUserAddresses(user_id);
-            console.log(accounUser.private_metadata);
-            console.log(accounUser.private_metadata.key);
+            console.log(accountUser.private_metadata);
+            console.log(accountUser.private_metadata.key);
             let userType = {
                 id: accountUser.id,
                 privateMetadata: [{
