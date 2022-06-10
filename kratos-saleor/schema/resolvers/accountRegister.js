55f1c2a13d4b66be9651e19916342e1f1295cff2
rexthony <rexthonyy@gmail.com>
2022-06-10 12:13:26 +0100

    update

--- kratos-saleor/schema/resolvers/accountRegister.js
+++ kratos-saleor/schema/resolvers/accountRegister.js
@@ -113,6 +113,7 @@ async function sendEmailConfirmation(redirectUrl, result) {
             };
 
             let token = jwt.sign(confirmationData, process.env.AUTHORIZATION_TOKEN_SECRET, { subject: user_id + "", expiresIn: process.env.JWT_TTL_CONFIRM_EMAIL });
+            console.log(token);
             let payload = {
                 requestType,
                 redirectUrl,
