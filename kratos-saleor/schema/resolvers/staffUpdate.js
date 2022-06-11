285eae5d99972307a407beda6955c658a820f522
rexthony <rexthonyy@gmail.com>
2022-06-11 17:33:53 +0100

    update

--- kratos-saleor/schema/resolvers/staffUpdate.js
+++ kratos-saleor/schema/resolvers/staffUpdate.js
@@ -1,5 +1,113 @@
-module.exports = async(parent, args, context) => {
+const pgKratosQueries = require("../../postgres/kratos-queries");
+const { isEmailValid } = require("../../libs/util");
+const { getGraphQLUserById } = require("./lib");
+
+module.exports = (parent, args, context) => {
     return new Promise((resolve, reject) => {
-        resolve(null);
+        if (!context.user) return resolve(getGraphQLOutput("authorization-bearer", "Please enter a valid authorization header", "JWT_INVALID_TOKEN", null, null));
+        const authUser = context.user;
+
+        let userId = args.id;
+        let input = args.input;
+
+        let groupsToAdd = args.input.addGroups;
+        let groupsToRemove = args.input.removeGroups;
+
+        if (args.input.email)
+            if (!isEmailValid(args.input.email)) return resolve(getGraphQLOutput("email", "Email format not supported", "INVALID_CREDENTIALS", null, null));
+
+        let inputValue = [userId];
+        let { values, whereClause } = getValuesForStaffUpdateFromInput(inputValue, input);
+
+        pgKratosQueries.updateAccountUserById(values, whereClause, async result => {
+            if (result.err) { console.log(result.err); return resolve(getGraphQLOutput("graphql error", "Failed to update staff", "GRAPHQL_ERROR", null, null)); }
+            let result = await updateStaffGroup(userId, groupsToAdd, groupsToRemove);
+            return resolve(result);
+        });
+    });
+}
+
+
+function getGraphQLOutput(field, message, code, addressType, user) {
+    return {
+        user: user,
+        errors: [{
+            field,
+            message,
+            code,
+            addressType
+        }],
+        staffErrors: [{
+            field,
+            message,
+            code,
+            addressType
+        }]
+    };
+}
+
+function getValuesForStaffUpdateFromInput(values, input) {
+    let whereClause = "";
+
+    if (input.firstName) {
+        values.push(input.firstName);
+        if (whereClause) whereClause += ", ";
+        whereClause += `first_name=$${values.length}`;
+    }
+    if (input.lastName) {
+        values.push(input.lastName);
+        if (whereClause) whereClause += ", ";
+        whereClause += `last_name=$${values.length}`;
+    }
+    if (input.email) {
+        values.push(input.email);
+        if (whereClause) whereClause += ", ";
+        whereClause += `email=$${values.length}`;
+    }
+    if (input.isActive) {
+        values.push(input.isActive);
+        if (whereClause) whereClause += ", ";
+        whereClause += `is_active=$${values.length}`;
+    }
+    if (input.note) {
+        values.push(input.note);
+        if (whereClause) whereClause += ", ";
+        whereClause += `note=$${values.length}`;
+    }
+
+    return { values, whereClause };
+}
+
+
+function updateStaffGroup(staffId, groupsToAdd, groupsToRemove) {
+    return new Promise(async(resolve) => {
+
+        try {
+            let payload = {
+                staffId,
+                groupsToAdd,
+                groupsToRemove
+            };
+
+            let res = await fetch(process.env.PERMISSIONS_APP_URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    query: ``,
+                    variables: payload
+                })
+            });
+
+            let json = res.json();
+
+            console.log(json);
+        } catch (err) {
+            console.log(err);
+        }
+
+        let graphQLUser = await getGraphQLUserById(staffId);
+        resolve(getGraphQLOutput("", "", "INVALID", null, graphQLUser));
     });
 }
