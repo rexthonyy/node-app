89d184dc672965f31df093abc9e96e5ae854b78d
rexthony <rexthonyy@gmail.com>
2022-06-13 16:23:31 +0100

    update

--- kratos-saleor/schema/resolvers/accountAddressUpdate.js
+++ kratos-saleor/schema/resolvers/accountAddressUpdate.js
@@ -150,7 +150,7 @@ function getValuesForAccountAddressUpdateFromInput(values, input) {
 function getAddressOwnerId(id) {
     return new Promise(resolve => {
         pgKratosQueries.getAccountUserAddressesByAddressId([id], result => {
-            if (result.err || result.res.length == 0) resolve(-1);
+            if (result.err || result.res.length == 0) return resolve(-1);
             resolve(result.res[0].user_id);
         });
     });
