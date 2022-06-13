9219339b80a7d5fcd8935335c4cc63dede40a945
rexthony <rexthonyy@gmail.com>
2022-06-13 15:54:41 +0100

    update

--- kratos-saleor/libs/consts.js
+++ kratos-saleor/libs/consts.js
@@ -2,6 +2,12 @@ const db = {
     account_user: "account_user",
     account_address: "account_address",
     account_user_addresses: "account_user_addresses",
+
+    auth_group: "auth_group",
+    auth_permissions: "auth_permission",
+    auth_group_permissions: "auth_group_permissions",
+    account_user_groups: "account_user_groups",
+    account_user_user_permissions: "account_user_user_permissions"
 };
 
 module.exports = {
