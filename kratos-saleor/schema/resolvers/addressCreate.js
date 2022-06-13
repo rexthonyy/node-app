4d6d13ec907cc46c22c3cfcad488edf8fa76f844
rexthony <rexthonyy@gmail.com>
2022-06-13 17:09:23 +0100

    update

--- kratos-saleor/schema/resolvers/addressCreate.js
+++ kratos-saleor/schema/resolvers/addressCreate.js
@@ -1,7 +1,6 @@
 const pgKratosQueries = require("../../postgres/kratos-queries");
 const { getGraphQLUserById } = require("./lib");
 const userPermissionGroupHasAccess = require("./lib/userPermissionGroupHasAccess");
-const getAddressOwnerId = require("./lib/getAddressOwnerId");
 
 module.exports = async(parent, args, context) => {
     return new Promise(async resolve => {
@@ -37,34 +36,37 @@ function addressCreate(args) {
             input.cityArea
         ];
 
-        pgKratosQueries.createAccountAddress(values, result => {
-            if (result.err || result.res.length == 0) return resolve(getGraphQLOutput("graphql error", "Failed to add account address", "GRAPHQL_ERROR", null, null, null));
-            let accountAddress = result.res[0];
-            pgKratosQueries.createAccountUserAddress([userId, accountAddress.id], async result => {
-                if (result.err || result.res.length == 0) return resolve(getGraphQLOutput("graphql error", "Failed to add account address to user", "GRAPHQL_ERROR", null, null, null));
-                let graphQLUser = await getGraphQLUserById(userId);
-                let defaultBillingAddress = graphQLUser.defaultBillingAddress;
-                let isDefaultBillingAddress = defaultBillingAddress ? (defaultBillingAddress.id == accountAddress.id) : false;
-                let defaultShippingAddress = graphQLUser.defaultShippingAddress;
-                let isDefaultShippingAddress = defaultShippingAddress ? (defaultShippingAddress.id == accountAddress.id) : false;
-                let address = {
-                    id: accountAddress.id,
-                    firstName: accountAddress.first_name,
-                    lastName: accountAddress.last_name,
-                    companyName: accountAddress.company_name,
-                    streetAddress1: accountAddress.street_address_1,
-                    streetAddress2: accountAddress.street_address_2,
-                    city: accountAddress.city,
-                    cityArea: accountAddress.city_area,
-                    postalCode: accountAddress.postal_code,
-                    country: accountAddress.country,
-                    countryArea: accountAddress.country_area,
-                    phone: accountAddress.phone,
-                    isDefaultShippingAddress,
-                    isDefaultBillingAddress
-                };
+        pgKratosQueries.getUserById([userId], result => {
+            if (result.err || result.res.length == 0) return resolve(getGraphQLOutput("user_id", "User not found", "NOT_FOUND", null, null, null));
+            pgKratosQueries.createAccountAddress(values, result => {
+                if (result.err || result.res.length == 0) return resolve(getGraphQLOutput("graphql error", "Failed to add account address", "GRAPHQL_ERROR", null, null, null));
+                let accountAddress = result.res[0];
+                pgKratosQueries.createAccountUserAddress([userId, accountAddress.id], async result => {
+                    if (result.err || result.res.length == 0) return resolve(getGraphQLOutput("graphql error", "Failed to add account address to user", "GRAPHQL_ERROR", null, null, null));
+                    let graphQLUser = await getGraphQLUserById(userId);
+                    let defaultBillingAddress = graphQLUser.defaultBillingAddress;
+                    let isDefaultBillingAddress = defaultBillingAddress ? (defaultBillingAddress.id == accountAddress.id) : false;
+                    let defaultShippingAddress = graphQLUser.defaultShippingAddress;
+                    let isDefaultShippingAddress = defaultShippingAddress ? (defaultShippingAddress.id == accountAddress.id) : false;
+                    let address = {
+                        id: accountAddress.id,
+                        firstName: accountAddress.first_name,
+                        lastName: accountAddress.last_name,
+                        companyName: accountAddress.company_name,
+                        streetAddress1: accountAddress.street_address_1,
+                        streetAddress2: accountAddress.street_address_2,
+                        city: accountAddress.city,
+                        cityArea: accountAddress.city_area,
+                        postalCode: accountAddress.postal_code,
+                        country: accountAddress.country,
+                        countryArea: accountAddress.country_area,
+                        phone: accountAddress.phone,
+                        isDefaultShippingAddress,
+                        isDefaultBillingAddress
+                    };
 
-                return resolve(getGraphQLOutput("", "", "INVALID", "", graphQLUser, address));
+                    return resolve(getGraphQLOutput("", "", "INVALID", "", graphQLUser, address));
+                });
             });
         });
     });
