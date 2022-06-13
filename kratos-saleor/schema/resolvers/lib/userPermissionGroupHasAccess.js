b9244118127c710fd2fe34423208d40ea2cb56b4
rexthony <rexthonyy@gmail.com>
2022-06-13 16:37:08 +0100

    update

--- kratos-saleor/schema/resolvers/lib/userPermissionGroupHasAccess.js
+++ kratos-saleor/schema/resolvers/lib/userPermissionGroupHasAccess.js
@@ -1,5 +1,6 @@
 let userPermissionGroupHasAccess = (permissionGroups, permissions) => {
     let hasAccess = false;
+    console.log(permissionGroups.permissions);
     for (let group of permissionGroups) {
         if (group.permissions.find(permission => {
                 for (let requiredPermission of permissions) {
