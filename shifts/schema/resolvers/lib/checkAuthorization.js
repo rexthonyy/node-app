d95bf39a59e6f53b358211f96d815f2e378a3c18
rexthony <rexthonyy@gmail.com>
2022-06-17 14:05:23 +0100

    update

--- shifts/schema/resolvers/lib/checkAuthorization.js
+++ shifts/schema/resolvers/lib/checkAuthorization.js
@@ -1,10 +1,12 @@
-module.exports = context => {
+let checkAuthorization = context => {
     if (!context.user) return returnData(false, null, "failed", "Please enter a valid authorization header");
     const authUser = context.user;
     if (!authUser.isStaff) return returnData(false, null, "failed", "Staff only. Non staff cannot have access");
     return returnData(true, authUser)
-}
+};
 
 function returnData(isAuthorized, authUser, status, message) {
     return { isAuthorized, authUser, status, message };
 }
+
+module.exports = checkAuthorization;
