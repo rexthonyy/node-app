0d41396685af9c552d82c8f321377ecd50fc8c68
rexthony <rexthonyy@gmail.com>
2022-06-17 14:06:31 +0100

    update

--- shifts/schema/resolvers/lib/index.js
+++ shifts/schema/resolvers/lib/index.js
@@ -1,5 +1,7 @@
 const getGraphQLUserById = require('./getGraphQLUserById');
+const checkAuthorization = require('./checkAuthorization');
 
 module.exports = {
-    getGraphQLUserById
+    getGraphQLUserById,
+    checkAuthorization
 };
