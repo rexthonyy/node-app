dc795b2377af0f12ca044b31ae7ece696227fb6b
rexthony <rexthonyy@gmail.com>
2022-06-21 12:51:59 +0100

    update

--- shifts/schema/resolvers/updateSettings.js
+++ shifts/schema/resolvers/updateSettings.js
@@ -7,7 +7,10 @@ module.exports = async(parent, args, context) => {
         let { isAuthorized, authUser, status, message } = checkAuthorization(context);
         if (!isAuthorized) return resolve(getGraphQLOutput(status, message, null));
 
-        resolve(await getSettings());
+        let key = args.key;
+        let value = args.value;
+
+        resolve(await updateSetting(key, value));
     });
 }
 
@@ -19,17 +22,36 @@ function getGraphQLOutput(status, message, result) {
     };
 }
 
-function getSettings() {
-    return new Promise(resolve => {
-        shiftQueries.getAllSettings(result => {
-            if (result.err) return resolve(getGraphQLOutput("failed", result.err, null));
-            if (result.res.length == 0) return resolve(getGraphQLOutput("failed", "Settings unintialized", null));
-            let settings = result.res;
-            let settingsRes = {};
+function updateSetting(key, value) {
+    return new Promise(async resolve => {
+        try {
+            let settings = await getAllSettings();
+            let isFound = false;
             for (let s of settings) {
-                settingsRes[s.key] = s.value;
+                if (s.key == key) isFound = true;
             }
-            if (result.res.length == 0) return resolve(getGraphQLOutput("success", "Settings fetch successful", settingsRes));
+            if (!isFound) return resolve(getGraphQLOutput("failed", "Setting not found", null));
+            shiftQueries.updateSetting([key, value], "value=$2", "key=$1", async result => {
+                if (result.err) return resolve(getGraphQLOutput("failed", result.err, null));
+                let updatedSettings = await getAllSettings();
+                let settingsRes = {};
+                for (let s of updatedSettings) {
+                    settingsRes[s.key] = s.value;
+                }
+                return resolve(getGraphQLOutput("success", "Settings update successful", settingsRes));
+            });
+        } catch (err) {
+            return resolve(err);
+        }
+    });
+}
+
+function getAllSettings() {
+    return new Promise((resolve, reject) => {
+        shiftQueries.getAllSettings(result => {
+            if (result.err) return reject(getGraphQLOutput("failed", result.err, null));
+            if (result.res.length == 0) return reject(getGraphQLOutput("failed", "Settings unintialized", null));
+            resolve(result.res);
         });
     });
 }
