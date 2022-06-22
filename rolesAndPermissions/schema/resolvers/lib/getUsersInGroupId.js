98fecbcd562f946a9838e7e99d922505b37df276
rexthony <rexthonyy@gmail.com>
2022-06-22 17:19:11 +0100

    update

--- rolesAndPermissions/schema/resolvers/lib/getUsersInGroupId.js
+++ rolesAndPermissions/schema/resolvers/lib/getUsersInGroupId.js
@@ -13,12 +13,20 @@ let getUsersInGroupId = (groupId) => {
 
             console.log(accountUserGroupRows);
             for (let accountUser of accountUserGroupRows) {
-                let graphQLUser = await getGraphQLUserById(Number(accountUser.user_id));
+                let graphQLUser = await getUser(accountUser.user_id);
                 users.push(graphQLUser);
             }
+
             resolve(users);
         });
     });
 }
 
+function getUser(userId) {
+    return new Promise(async resolve => {
+        console.log("user id: ", userId);
+        let graphQLUser = await getGraphQLUserById(Number(accountUser.user_id));
+        resolve(graphQLUser);
+    });
+}
 module.exports = getUsersInGroupId;
