8924bae6138963725691dd7908ad6062b4c7d693
rexthony <rexthonyy@gmail.com>
2022-06-22 17:34:48 +0100

    update

--- rolesAndPermissions/schema/resolvers/lib/getUsersInGroupId.js
+++ rolesAndPermissions/schema/resolvers/lib/getUsersInGroupId.js
@@ -2,6 +2,7 @@ const permissionsdbQueries = require('../../../postgres/permissionsdb-queries');
 const pgKratosQueries = require('../../../postgres/kratos-queries');
 const getUserPermissions = require('./getUserPermissions');
 const getUserPermissionGroups = require('./getUserPermissionGroups');
+const getAuthGroupPermissionsByGroupId = require('./getAuthGroupPermissionsByGroupId');
 
 let getUsersInGroupId = (groupId) => {
     return new Promise(resolve => {
@@ -137,5 +138,60 @@ function getDefaultAddress(address_id, addresses) {
     }
     return defaultAddress;
 }
+
+let getUserPermissionGroups = (userId) => {
+    return new Promise(resolve => {
+        pgPermissionsQueries.getAccountUserGroupsByUserId([userId], result => {
+            if (result.err || result.res.length == 0) {
+                return resolve([]);
+            }
+
+            let accountUserGroupRows = result.res;
+            const numRows = accountUserGroupRows.length;
+            let countGroups = -1;
+            let userPermissionGroups = [];
+
+            accountUserGroupRows.forEach(row => {
+                pgPermissionsQueries.getAuthGroupById([row.group_id], async result => {
+                    if (result.err || result.res.length == 0) {
+                        checkPermissionGroupComplete();
+                    } else {
+                        let authGroup = result.res[0];
+
+                        let permissions = await getAuthGroupPermissionsByGroupId(authGroup.id);
+                        let userCanManage = false;
+
+                        for (let i = 0, j = permissions.length; i < j; i++) {
+                            if (permissions[i].code == "MANAGE_USERS") {
+                                userCanManage = true;
+                                break;
+                            }
+                        }
+                        let users = null;
+
+                        userPermissionGroups.push({
+                            id: authGroup.id,
+                            name: authGroup.name,
+                            users,
+                            permissions,
+                            userCanManage
+                        });
+
+                        checkPermissionGroupComplete();
+                    }
+                });
+            });
+
+            checkPermissionGroupComplete();
+
+            function checkPermissionGroupComplete() {
+                countGroups++;
+                if (countGroups == numRows) {
+                    return resolve(userPermissionGroups);
+                }
+            }
+        });
+    });
+};
 
 module.exports = getUsersInGroupId;
