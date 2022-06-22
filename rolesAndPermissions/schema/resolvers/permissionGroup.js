3fd61db3131e4acbcecce2d30628191e63cd43f3
rexthony <rexthonyy@gmail.com>
2022-06-22 16:23:12 +0100

    update

--- rolesAndPermissions/schema/resolvers/permissionGroup.js
+++ rolesAndPermissions/schema/resolvers/permissionGroup.js
@@ -16,7 +16,7 @@ module.exports = async(parent, args, context) => {
 
         let group_id = args.id;
         let permissions = ["MANAGE_STAFF"];
-
+        console.log(group_id);
         if (userHasAccess(authUser.userPermissions, permissions) || userPermissionGroupHasAccess(authUser.permissionGroups, permissions)) {
             console.log(authUser.userPermissions);
             permissionsdbQueries.getAuthGroupById([group_id], result => {
