bb36f9f214ebaf355ec77518f7688213d0151931
rexthony <rexthonyy@gmail.com>
2022-06-22 16:52:14 +0100

    update

--- rolesAndPermissions/schema/resolvers/lib/getUsersInGroupId.js
+++ rolesAndPermissions/schema/resolvers/lib/getUsersInGroupId.js
@@ -1,21 +1,23 @@
 const permissionsdbQueries = require('../../../postgres/permissionsdb-queries');
 const getGraphQLUserById = require('./getGraphQLUserById');
 
-let getUsersInGroupId = (groupId, cb) => {
-    permissionsdbQueries.getAccountUserGroupsByGroupId([groupId], async result => {
-        if (result.err || result.res.length == 0) {
-            return cb([]);
-        }
+let getUsersInGroupId = (groupId) => {
+    return new Promise(resolve => {
+        permissionsdbQueries.getAccountUserGroupsByGroupId([groupId], async result => {
+            if (result.err || result.res.length == 0) {
+                return resolve([]);
+            }
 
-        let accountUserGroupRows = result.res;
-        let users = [];
+            let accountUserGroupRows = result.res;
+            let users = [];
 
-        for (let accountUser of accountUserGroupRows) {
-            console.log(accountUser.user_id);
-            users.push(await getGraphQLUserById(accountUser.user_id));
-        }
-        cb(users);
-
+            console.log(accountUserRows);
+            for (let accountUser of accountUserGroupRows) {
+                let graphQLUser = await getGraphQLUserById(accountUser.user_id);
+                users.push(graphQLUser);
+            }
+            resolve(users);
+        });
     });
 }
 
