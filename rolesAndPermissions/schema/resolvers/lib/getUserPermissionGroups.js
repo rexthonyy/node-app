b6e3c80d17f38ae8b65d8b304ecffc7f45d3b833
rexthony <rexthonyy@gmail.com>
2022-06-22 16:35:36 +0100

    update

--- rolesAndPermissions/schema/resolvers/lib/getUserPermissionGroups.js
+++ rolesAndPermissions/schema/resolvers/lib/getUserPermissionGroups.js
@@ -1,59 +1,59 @@
-const permissionsdbQueries = require('../../../postgres/permissionsdb-queries');
+const pgPermissionsQueries = require('../../../postgres/permissionsdb-queries');
 const getAuthGroupPermissionsByGroupId = require('./getAuthGroupPermissionsByGroupId');
 const getUsersInGroupId = require('./getUsersInGroupId');
 
-let getUserPermissionGroups = (authUser, userId, cb) => {
-    permissionsdbQueries.getAccountUserGroupsByUserId([userId], result => {
-        if (result.err || result.res.length == 0) {
-            return cb([]);
-        }
+let getUserPermissionGroups = (userId) => {
+    return new Promise(resolve => {
+        pgPermissionsQueries.getAccountUserGroupsByUserId([userId], result => {
+            if (result.err || result.res.length == 0) {
+                return resolve([]);
+            }
 
-        let accountUserGroupRows = result.res;
-        const numRows = accountUserGroupRows.length;
-        let countGroups = -1;
-        let userPermissionGroups = [];
+            let accountUserGroupRows = result.res;
+            const numRows = accountUserGroupRows.length;
+            let countGroups = -1;
+            let userPermissionGroups = [];
 
-        accountUserGroupRows.forEach(row => {
-            permissionsdbQueries.getAuthGroupById([row.group_id], result => {
-                if (result.err || result.res.length == 0) {
-                    checkPermissionGroupComplete();
-                } else {
-                    let authGroup = result.res[0];
-                    getAuthGroupPermissionsByGroupId(authGroup.id, permissions => {
-                        let authUserPermissions = authUser ? (authUser.userPermissions ? authUser.userPermissions : []) : [];
+            accountUserGroupRows.forEach(row => {
+                pgPermissionsQueries.getAuthGroupById([row.group_id], async result => {
+                    if (result.err || result.res.length == 0) {
+                        checkPermissionGroupComplete();
+                    } else {
+                        let authGroup = result.res[0];
+
+                        let permissions = await getAuthGroupPermissionsByGroupId(authGroup.id);
                         let userCanManage = false;
 
-                        for (let i = 0, j = authUserPermissions.length; i < j; i++) {
-                            if (authUserPermissions[i].code == "MANAGE_USERS") {
+                        for (let i = 0, j = permissions.length; i < j; i++) {
+                            if (permissions[i].code == "MANAGE_USERS") {
                                 userCanManage = true;
                                 break;
                             }
                         }
+                        let users = await getUsersInGroupId(authGroup.id);
 
-                        getUsersInGroupId(authGroup.id, users => {
-                            userPermissionGroups.push({
-                                id: authGroup.id,
-                                name: authGroup.name,
-                                users,
-                                permissions,
-                                userCanManage
-                            });
-
-                            checkPermissionGroupComplete();
+                        userPermissionGroups.push({
+                            id: authGroup.id,
+                            name: authGroup.name,
+                            users,
+                            permissions,
+                            userCanManage
                         });
-                    });
-                }
+
+                        checkPermissionGroupComplete();
+                    }
+                });
             });
-        });
 
-        checkPermissionGroupComplete();
+            checkPermissionGroupComplete();
 
-        function checkPermissionGroupComplete() {
-            countGroups++;
-            if (countGroups == numRows) {
-                return cb(userPermissionGroups);
+            function checkPermissionGroupComplete() {
+                countGroups++;
+                if (countGroups == numRows) {
+                    return resolve(userPermissionGroups);
+                }
             }
-        }
+        });
     });
 };
 
