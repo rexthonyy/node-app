bb2453b29a095c7c1460b45d3a44266078959c8b
rexthony <rexthonyy@gmail.com>
2022-06-25 17:15:12 +0100

    update

--- products/schema/resolvers/productTypeCreate.js
+++ products/schema/resolvers/productTypeCreate.js
@@ -1,6 +1,5 @@
 const {
     checkAuthorization,
-    getGraphQLProductById,
     userPermissionGroupHasAccess,
     userHasAccess
 } = require('./lib');
@@ -73,7 +72,9 @@ function productTypeCreate(authUser, args) {
             let productType = result.res[0];
             await addProductAttributes(productType, productAttributes);
             await addVariantAttributes(productType, variantAttributes);
+            console.log("product type created");
             let graphQLProductType = await getGraphQLProductTypeById(productType.id);
+            console.log(".....");
             console.log(graphQLProductType);
             resolve(getGraphQLOutput("producttype", "Product type created", "GRAPHQL_ERROR", null, null, graphQLProductType));
         });
