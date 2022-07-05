37b802203340cfe339e6d765fd5562bd20690f64
rexthony <rexthonyy@gmail.com>
2022-07-05 12:28:25 +0100

    update

--- products/schema/resolvers/Mutation/productBulkDelete.js
+++ products/schema/resolvers/Mutation/productBulkDelete.js
@@ -4,6 +4,7 @@ const {
     userPermissionGroupHasAccess,
     userHasAccess
 } = require('../lib');
+//.
 const productQueries = require("../../../postgres/product-queries");
 
 module.exports = async(parent, args, context) => {
