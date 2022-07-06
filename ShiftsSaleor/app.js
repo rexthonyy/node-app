8aeef5291e4e5c61cfed789f5c3d781bc20b898d
rexthony <rexthonyy@gmail.com>
2022-07-06 17:45:00 +0100

    update

--- ShiftsSaleor/app.js
+++ ShiftsSaleor/app.js
@@ -18,7 +18,7 @@ const app = express();
 // let port = process.env.PORT || 1000;
 // var lesServer = app.listen(port, function() {
 //     console.log("Listening on port %s...", lesServer.address().port);
-// });
+// });;
 async function main() {
     const server = new ApolloServer({
         schema: schema,
