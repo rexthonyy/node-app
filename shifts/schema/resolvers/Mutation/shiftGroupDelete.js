5fa57bb95c8b3bbd29a1bc394fb8a645281033b7
rexthony <rexthonyy@gmail.com>
2022-07-07 17:27:05 +0100

    update

--- shifts/schema/resolvers/Mutation/shiftGroupDelete.js
+++ shifts/schema/resolvers/Mutation/shiftGroupDelete.js
@@ -4,7 +4,7 @@ const { checkAuthorization, userPermissionGroupHasAccess } = require('../lib');
 module.exports = async(parent, args, context) => {
     return new Promise(async resolve => {
         let { isAuthorized, authUser, status, message } = checkAuthorization(context);
-        if (!isAuthorized) return resolve(getGraphQLOutput(status, message));
+        if (!isAuthorized) return resolve(getGraphQLOutput(status, message, "INVALID", null));
 
         let channelId = args.channelId;
         let shiftGroupId = args.shiftGroupId;
@@ -14,28 +14,45 @@ module.exports = async(parent, args, context) => {
         } else if (userPermissionGroupHasAccess(authUser.permissionGroups, ["MANAGE_STAFF"])) {
             resolve(await shiftGroupDelete(channelId, shiftGroupId));
         } else {
-            resolve(getGraphQLOutput("failed", "You do not have permission to perform this operation"));
+            resolve(getGraphQLOutput("permission", "You do not have permission to perform this operation", "INVALID", null));
         }
     });
 }
 
-function getGraphQLOutput(status, message) {
+function getGraphQLOutput(field, message, code, shiftGroup = null) {
     return {
-        status,
-        message
-    };
+        errors: [{
+            field,
+            message,
+            code
+        }],
+        shiftGroup
+    }
 }
 
 function shiftGroupDelete(channelId, shiftGroupId) {
     return new Promise(async resolve => {
-        await deleteAssignedShiftActivitiesInShiftGroup(shiftGroupId);
-        await deleteOpenShiftActivitiesInShiftGroup(shiftGroupId);
-        await deleteUserTimeOffsInShiftGroup(shiftGroupId);
-        await deleteAssignedShiftsInShiftGroup(shiftGroupId);
-        await deleteOpenShiftsInShiftGroup(shiftGroupId);
-        await deleteShiftGroupMembersInShiftGroup(shiftGroupId);
-        await deleteShiftGroup(shiftGroupId);
-        resolve(getGraphQLOutput("success", "Shift group deleted"));
+        shiftQueries.getShiftGroupById([shiftGroupId], result => {
+            if (result.err) return resolve(getGraphQLOutput("shiftGroupId", JSON.stringify(result.err), "GRAPHQL_ERROR", null));
+            if (result.res.length == 0) return resolve(getGraphQLOutput("shiftGroupId", "Shift group not found", "NOT_FOUND", null));
+            let shiftGroupRow = result.res[0];
+            let shiftGroup = {
+                channelId: shiftGroupRow.channel_id,
+                shiftGroupId: shiftGroupRow.id,
+                name: shiftGroupRow.name
+            };
+            await deleteAssignedShiftActivitiesInShiftGroup(shiftGroupId);
+            await deleteOpenShiftActivitiesInShiftGroup(shiftGroupId);
+            await deleteUserTimeOffsInShiftGroup(shiftGroupId);
+            await deleteAssignedShiftsInShiftGroup(shiftGroupId);
+            await deleteOpenShiftsInShiftGroup(shiftGroupId);
+            await deleteShiftGroupMembersInShiftGroup(shiftGroupId);
+            await deleteShiftGroup(shiftGroupId);
+            resolve({
+                errors: [],
+                shiftGroup
+            })
+        });
     });
 }
 
