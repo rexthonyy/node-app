37fa36c9848d587b6c46e386ba277d4d39372556
rexthony <rexthonyy@gmail.com>
2022-07-07 16:30:44 +0100

    update

--- shifts/schema/resolvers/Mutation/openShiftDelete.js
+++ shifts/schema/resolvers/Mutation/openShiftDelete.js
@@ -4,7 +4,7 @@ const { checkAuthorization, userPermissionGroupHasAccess, getGraphQLUserById } =
 module.exports = async(parent, args, context) => {
     return new Promise(async resolve => {
         let { isAuthorized, authUser, status, message } = checkAuthorization(context);
-        if (!isAuthorized) return resolve(getGraphQLOutput(status, message, null));
+        if (!isAuthorized) return resolve(getGraphQLOutput(status, message, "INVALID", null));
 
         let channelId = args.channelId;
         let shiftGroupId = args.shiftGroupId;
@@ -15,34 +15,43 @@ module.exports = async(parent, args, context) => {
         } else if (userPermissionGroupHasAccess(authUser.permissionGroups, ["MANAGE_STAFF"])) {
             resolve(await openShiftDelete(openShiftId, channelId, shiftGroupId));
         } else {
-            resolve(getGraphQLOutput("failed", "You do not have permission to perform this operation", null));
+            resolve(getGraphQLOutput("permission", "You do not have permission to perform this operation", "GRAPHQL_ERROR", null));
         }
     });
 }
 
-function getGraphQLOutput(status, message, result) {
+function getGraphQLOutput(field, message, code, openShift = null) {
     return {
-        status,
-        message,
-        result
-    };
+        errors: [{
+            field,
+            message,
+            code
+        }],
+        openShift
+    }
 }
 
 function openShiftDelete(openShiftId, channelId, shiftGroupId) {
     return new Promise(resolve => {
         shiftQueries.getShiftGroupById([shiftGroupId], async result => {
-            if (result.err) return resolve(getGraphQLOutput("failed", result.err, null));
-            if (result.res.length == 0) return resolve(getGraphQLOutput("failed", "Shift group does not exist", null));
+            if (result.err) return resolve(getGraphQLOutput("shiftGroupId", JSON.stringify(result.err), "GRAPHQL_ERROR", null));
+            if (result.res.length == 0) return resolve(getGraphQLOutput("failed", "Shift group not found", "NOT_FOUND", null));
             shiftQueries.getOpenShifts([openShiftId], "id=$1", async result => {
-                if (result.err) return resolve(getGraphQLOutput("failed", result.err, null));
-                if (result.res.length == 0) return resolve(getGraphQLOutput("failed", "open shift does not exist", null));
-                let shift = await getGraphQLOpenShift(openShiftId);
-
-                shiftQueries.deleteOpenShiftActivities([openShiftId], "open_shift_id=$1", result => {
-                    shiftQueries.deleteOpenShift([openShiftId], "id=$1", result => {
-                        resolve(getGraphQLOutput("success", "Open shift deleted", shift));
+                if (result.err) return resolve(getGraphQLOutput("openShiftId", JSON.stringify(result.err), "GRAPHQL_ERROR", null));
+                if (result.res.length == 0) return resolve(getGraphQLOutput("openShiftId", "open shift does not exist", "NOT_FOUND", null));
+                try {
+                    let shift = await getGraphQLOpenShift(openShiftId);
+                    shiftQueries.deleteOpenShiftActivities([openShiftId], "open_shift_id=$1", result => {
+                        shiftQueries.deleteOpenShift([openShiftId], "id=$1", result => {
+                            resolve({
+                                errors: [],
+                                openShift: shift
+                            });
+                        });
                     });
-                });
+                } catch (err) {
+                    return resolve(getGraphQLOutput("openshift", err, "GRAPHQL_ERROR", null));
+                }
             });
         });
     });
