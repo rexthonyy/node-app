d393ef89b700ea915a654c9246abac19992199af
rexthony <rexthonyy@gmail.com>
2022-07-07 15:20:22 +0100

    update

--- shifts/schema/resolvers/Mutation/assignedShiftDelete.js
+++ shifts/schema/resolvers/Mutation/assignedShiftDelete.js
@@ -4,7 +4,7 @@ const { checkAuthorization, userPermissionGroupHasAccess, getGraphQLUserById } =
 module.exports = async(parent, args, context) => {
     return new Promise(async resolve => {
         let { isAuthorized, authUser, status, message } = checkAuthorization(context);
-        if (!isAuthorized) return resolve(getGraphQLOutput(status, message, null));
+        if (!isAuthorized) return resolve(getGraphQLOutput(status, message, "INVALID", null));
 
         let assignedShiftId = args.assignedShiftId;
         let channelId = args.channelId;
@@ -20,27 +20,33 @@ module.exports = async(parent, args, context) => {
     });
 }
 
-function getGraphQLOutput(status, message, result) {
+function getGraphQLOutput(field, message, code, shift = null) {
     return {
-        status,
-        message,
-        result
-    };
+        errors: [{
+            field,
+            message,
+            code
+        }],
+        shift
+    }
 }
 
 function assignedShiftDelete(assignedShiftId, channelId, shiftGroupId) {
     return new Promise(resolve => {
         shiftQueries.getShiftGroupById([shiftGroupId], async result => {
-            if (result.err) return resolve(getGraphQLOutput("failed", result.err, null));
-            if (result.res.length == 0) return resolve(getGraphQLOutput("failed", "Shift group does not exist", null));
+            if (result.err) return resolve(getGraphQLOutput("shiftGroupId", JSON.stringify(result.err), "GRAPHQL_ERROR", null));
+            if (result.res.length == 0) return resolve(getGraphQLOutput("failed", "Shift group does not exist", "NOT_FOUND", null));
             shiftQueries.getAssignedShifts([assignedShiftId], "id=$1", async result => {
-                if (result.err) return resolve(getGraphQLOutput("failed", result.err, null));
-                if (result.res.length == 0) return resolve(getGraphQLOutput("failed", "Assigned shift does not exist", null));
+                if (result.err) return resolve(getGraphQLOutput("assignedShiftId", JSON.stringify(result.err), "GRAPHQL_ERROR", null));
+                if (result.res.length == 0) return resolve(getGraphQLOutput("failed", "Assigned shift does not exist", "NOT_FOUND", null));
                 let shift = await getGraphQLAssignedShift(assignedShiftId);
 
                 shiftQueries.deleteAssignedShiftActivities([assignedShiftId], "assigned_shift_id=$1", result => {
                     shiftQueries.deleteAssignedShift([assignedShiftId], "id=$1", result => {
-                        resolve(getGraphQLOutput("success", "Assigned shift deleted", shift));
+                        resolve({
+                            errors: [],
+                            assignedShift: shift
+                        });
                     });
                 });
             });
