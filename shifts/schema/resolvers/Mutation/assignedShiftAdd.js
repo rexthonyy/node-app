c2a04914d1e5edebe4deb4422e93788ff0872155
rexthony <rexthonyy@gmail.com>
2022-07-07 15:05:00 +0100

    update

--- shifts/schema/resolvers/Mutation/assignedShiftAdd.js
+++ shifts/schema/resolvers/Mutation/assignedShiftAdd.js
@@ -7,7 +7,7 @@ const { checkAuthorization, userPermissionGroupHasAccess, getGraphQLUserById } =
 module.exports = async(parent, args, context) => {
     return new Promise(async resolve => {
         let { isAuthorized, authUser, status, message } = checkAuthorization(context);
-        if (!isAuthorized) return resolve(getGraphQLOutput(status, message, null));
+        if (!isAuthorized) return resolve(getGraphQLOutput(status, message, "INVALID", null));
 
         let channelId = args.channelId;
         let shiftGroupId = args.shiftGroupId;
@@ -26,32 +26,35 @@ module.exports = async(parent, args, context) => {
         } else if (userPermissionGroupHasAccess(authUser.permissionGroups, ["MANAGE_STAFF"])) {
             resolve(await assignedShiftAdd(channelId, shiftGroupId, userId, color, label, note, is24Hours, startTime, endTime, unpaidBreak, shiftActivities));
         } else {
-            resolve(getGraphQLOutput("failed", "You do not have permission to perform this operation", null));
+            resolve(getGraphQLOutput("failed", "You do not have permission to perform this operation", "INVALID", null));
         }
     });
 }
 
-function getGraphQLOutput(status, message, result) {
+function getGraphQLOutput(field, message, code, shift = null) {
     return {
-        status,
-        message,
-        result
-    };
+        errors: [{
+            field,
+            message,
+            code
+        }],
+        shift
+    }
 }
 
 function assignedShiftAdd(channelId, shiftGroupId, userId, color, label, note, is24Hours, startTime, endTime, unpaidBreak, shiftActivities) {
     return new Promise(resolve => {
         kratosQueries.getChannel([channelId], "id=$1", result => {
-            if (result.err) return resolve(getGraphQLOutput("failed", result.err, null));
-            if (result.res.length == 0) return resolve(getGraphQLOutput("failed", "Channel not found", null));
+            if (result.err) return resolve(getGraphQLOutput("channelId", JSON.stringify(result.err), "GRAPHQL_ERROR", null));
+            if (result.res.length == 0) return resolve(getGraphQLOutput("channelId", "Channel not found", "NOT_FOUND", null));
 
             shiftQueries.getShiftGroupById([shiftGroupId], async result => {
-                if (result.err) return resolve(getGraphQLOutput("failed", result.err, null));
-                if (result.res.length == 0) return resolve(getGraphQLOutput("failed", "Shift group does not exist", null));
+                if (result.err) return resolve(getGraphQLOutput("shiftGroupId", JSON.stringify(result.err), "GRAPHQL_ERROR", null));
+                if (result.res.length == 0) return resolve(getGraphQLOutput("shiftGroupId", "Shift group does not exist", "NOT_FOUND", null));
                 try {
-                    let user = await getGraphQLUserById(userId);
+                    await getGraphQLUserById(userId);
                 } catch (err) {
-                    return resolve(getGraphQLOutput("failed", err, null));
+                    return resolve(getGraphQLOutput("userId", err, "REQUIRED", null));
                 }
 
                 let values = [
@@ -69,7 +72,8 @@ function assignedShiftAdd(channelId, shiftGroupId, userId, color, label, note, i
                     unpaidBreakValue[unpaidBreak],
                 ];
                 shiftQueries.createAssignedShift(values, result => {
-                    if (result.err) { console.log(result.err); return resolve(getGraphQLOutput("failed", "Failed to create assigned shift", null)) };
+                    if (result.err) { console.log(result.err); return resolve(getGraphQLOutput("assignedshift", JSON.stringify(result.err), "GRAPHQL_ERROR", null)) };
+                    if (result.res.length == 0) return resolve(getGraphQLOutput("assignedshift", "Failed to create assigned shift", "REQUIRED", null));
                     let assignedShift = result.res[0];
                     let assignedShiftId = assignedShift.id;
                     const numAssignedShiftActivities = shiftActivities.length;
@@ -100,7 +104,10 @@ function assignedShiftAdd(channelId, shiftGroupId, userId, color, label, note, i
                         cursor++;
                         if (cursor == numAssignedShiftActivities) {
                             let shift = await getGraphQLAssignedShift(assignedShiftId);
-                            resolve(getGraphQLOutput("success", "Assigned shift created", shift));
+                            resolve({
+                                errors: [],
+                                assignedShift: shift
+                            })
                         }
                     }
 
