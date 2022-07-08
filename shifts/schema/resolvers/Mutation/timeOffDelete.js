7f5ecd7e6b3eb3aa5838f820d8d0d5d88206add7
rexthony <rexthonyy@gmail.com>
2022-07-08 13:06:57 +0100

    update

--- shifts/schema/resolvers/Mutation/timeOffDelete.js
+++ shifts/schema/resolvers/Mutation/timeOffDelete.js
@@ -4,7 +4,7 @@ const { checkAuthorization, userPermissionGroupHasAccess, getGraphQLUserById } =
 module.exports = async(parent, args, context) => {
     return new Promise(async resolve => {
         let { isAuthorized, authUser, status, message } = checkAuthorization(context);
-        if (!isAuthorized) return resolve(getGraphQLOutput(status, message, null));
+        if (!isAuthorized) return resolve(getGraphQLOutput(status, message, "INVALID", null));
 
         let channelId = args.channelId;
         let shiftGroupId = args.shiftGroupId;
@@ -15,30 +15,36 @@ module.exports = async(parent, args, context) => {
         } else if (userPermissionGroupHasAccess(authUser.permissionGroups, ["MANAGE_STAFF"])) {
             resolve(await timeOffDelete(timeOffId, channelId, shiftGroupId));
         } else {
-            resolve(getGraphQLOutput("failed", "You do not have permission to perform this operation", null));
+            resolve(getGraphQLOutput("permission", "You do not have permission to perform this operation", "INVALID", null));
         }
     });
 }
 
-function getGraphQLOutput(status, message, result) {
+function getGraphQLOutput(field, message, code, timeoff = null) {
     return {
-        status,
-        message,
-        result
-    };
+        errors: [{
+            field,
+            message,
+            code
+        }],
+        timeoff
+    }
 }
 
 function timeOffDelete(timeOffId, channelId, shiftGroupId) {
     return new Promise(resolve => {
         shiftQueries.getShiftGroupById([shiftGroupId], async result => {
-            if (result.err) return resolve(getGraphQLOutput("failed", result.err, null));
-            if (result.res.length == 0) return resolve(getGraphQLOutput("failed", "Shift group does not exist", null));
+            if (result.err) return resolve(getGraphQLOutput("shiftGroupId", JSON.stringify(result.err), "GRAPHQL_ERROR", null));
+            if (result.res.length == 0) return resolve(getGraphQLOutput("timeOffId", "Shift group does not exist", "NOT_FOUND", null));
             shiftQueries.getUserTimeOffs([timeOffId], "id=$1", async result => {
-                if (result.err) return resolve(getGraphQLOutput("failed", result.err, null));
-                if (result.res.length == 0) return resolve(getGraphQLOutput("failed", "time off does not exist", null));
+                if (result.err) return resolve(getGraphQLOutput("timeOffId", JSON.stringify(result.err), "GRAPHQL_ERROR", null));
+                if (result.res.length == 0) return resolve(getGraphQLOutput("timeOffId", "time off does not exist", "NOT_FOUND", null));
                 let timeoff = await getGraphQLUserTimeOff(timeOffId);
                 shiftQueries.deleteUserTimeOffs([timeOffId], "id=$1", result => {
-                    resolve(getGraphQLOutput("success", "Timeoff deleted", timeoff));
+                    resolve({
+                        errors: [],
+                        timeoff
+                    });
                 });
             });
         });
