6de23f772b044eaf607bcb5abe74b13f33c21b20
rexthony <rexthonyy@gmail.com>
2022-07-09 18:37:07 +0100

    update

--- ShiftsSaleor/app.js
+++ ShiftsSaleor/app.js
@@ -1,30 +1,17 @@
 require("dotenv").config();
 const express = require('express');
-const { ApolloServer } = require("apollo-server");
-const { graphqlHTTP } = require('express-graphql');
-const { applyMiddleware } = require('graphql-middleware');
-const middleware = require('./libs/middleware');
+const { ApolloServer } = require("apollo-server-express");
 const schema = require('./schema');
-const schemaWithMiddleware = applyMiddleware(schema, middleware)
 const app = express();
 
-// app.use('/graphql',
-//     graphqlHTTP(req => ({
-//         schema: schemaWithMiddleware,
-//         graphiql: true,
-//         context: req
-//     })));
+const server = new ApolloServer({
+    schema: schema,
+    uploads: false
+});
 
-// let port = process.env.PORT || 1000;
-// var lesServer = app.listen(port, function() {
-//     console.log("Listening on port %s...", lesServer.address().port);
-// });;
+server.applyMiddleware({ app });
+
 async function main() {
-    const server = new ApolloServer({
-        schema: schema,
-        uploads: false
-    });
-
     const { url } = await server.listen({ port: process.env.PORT });
     console.log(`🚀 Server ready at ${url}`);
 }
