5d3057a7b9616fb5f80c50e3ed97bdd5eb06daa7
rexthony <rexthonyy@gmail.com>
2022-07-12 13:02:35 +0100

    update

--- products/schema/resolvers/Mutation/productVariantStocksCreate.js
+++ products/schema/resolvers/Mutation/productVariantStocksCreate.js
@@ -9,33 +9,35 @@ const productQueries = require("../../../postgres/product-queries");
 module.exports = async(parent, args, context) => {
     return new Promise(async resolve => {
         let { isAuthorized, authUser, status, message } = checkAuthorization(context);
-        if (!isAuthorized) return resolve(getGraphQLOutput("authorization-header", message, "INVALID", null, null, null));
+        if (!isAuthorized) return resolve(getGraphQLOutput("authorization-header", message, "INVALID", null, null, -1, null));
 
         let accessPermissions = ["MANAGE_PRODUCTS"];
 
         if (userHasAccess(authUser.userPermissions, accessPermissions) || userPermissionGroupHasAccess(authUser.permissionGroups, accessPermissions)) {
             resolve(await productVariantStocksCreate(authUser, args));
         } else {
-            resolve(getGraphQLOutput("No access", "You do not have the necessary permissions required to perform this operation. Permissions required MANAGE_PRODUCTS", "INVALID", null, null, null));
+            resolve(getGraphQLOutput("No access", "You do not have the necessary permissions required to perform this operation. Permissions required MANAGE_PRODUCTS", "INVALID", null, null, -1, null));
         }
     });
 }
 
-function getGraphQLOutput(field, message, code, attributes, values, productVariant) {
+function getGraphQLOutput(field, message, code, attributes, values, index, productVariant) {
     return {
         errors: [{
             field,
             message,
             code,
             attributes,
-            values
+            values,
+            index
         }],
         bulkStockErrors: [{
             field,
             message,
             code,
             attributes,
-            values
+            values,
+            index
         }],
         productVariant
     }
@@ -66,13 +68,15 @@ function productVariantStocksCreate(authUser, args) {
             if (cursor == numStock) {
                 try {
                     let productVariant = await getGraphQLProductVariantById(variantId);
-                    return resolve({
+                    let res = {
                         errors,
                         bulkStockErrors: errors,
                         productVariant
-                    });
+                    };
+                    console.log(res);
+                    return resolve(res);
                 } catch (err) {
-                    resolve(getGraphQLOutput("productVariant", err, "GRAPHQL_ERROR", null, null, null));
+                    resolve(getGraphQLOutput("productVariant", err, "GRAPHQL_ERROR", null, null, 0, null));
                 }
             }
         }
@@ -82,11 +86,11 @@ function productVariantStocksCreate(authUser, args) {
 function createProductVariantStock(variantId, stock) {
     return new Promise((resolve, reject) => {
         productQueries.getWarehouse([stock.warehouse], "id=$1", result => {
-            if (result.err) return reject(getGraphQLOutput("stock.warehouse", JSON.stringify(result.err), "GRAPHQL_ERROR", null, null, null).errors);
-            if (result.res.length == 0) return reject(getGraphQLOutput("warehouse", "Warehouse not found", "GRAPHQL_ERROR", null, null, null).errors);
+            if (result.err) return reject(getGraphQLOutput("stock.warehouse", JSON.stringify(result.err), "GRAPHQL_ERROR", null, null, 0, null).errors);
+            if (result.res.length == 0) return reject(getGraphQLOutput("warehouse", "Warehouse not found", "GRAPHQL_ERROR", null, null, 0, null).errors);
             productQueries.createWarehouseStock([stock.quantity, variantId, stock.warehouse, 0], result => {
-                if (result.err) return reject(getGraphQLOutput("stock.warehouse", JSON.stringify(result.err), "GRAPHQL_ERROR", null, null, null).errors);
-                if (result.res.length == 0) return reject(getGraphQLOutput("warehouse", "Warehouse stock not created", "GRAPHQL_ERROR", null, null, null).errors);
+                if (result.err) return reject(getGraphQLOutput("stock.warehouse", JSON.stringify(result.err), "GRAPHQL_ERROR", null, null, 0, null).errors);
+                if (result.res.length == 0) return reject(getGraphQLOutput("warehouse", "Warehouse stock not created", "GRAPHQL_ERROR", null, null, 0, null).errors);
                 resolve();
             });
         });
