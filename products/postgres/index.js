79d3f28bdcfacb3824b3aa55d617e3f0766a4239
rexthony <rexthonyy@gmail.com>
2022-07-15 18:08:26 +0100

    update

--- products/postgres/index.js
+++ products/postgres/index.js
@@ -4,10 +4,13 @@ const productQueries = require("./product-queries");
 const shiftQueries = require("./shift-queries");
 
 module.exports = {
-    stop: async() => {
-        await kratosQueries.stop();
-        await permissionsdbQueries.stop();
-        await productQueries.stop();
-        await shiftQueries.stop();
+    stop: () => {
+        return new Promise(async resolve => {
+            await kratosQueries.stop();
+            await permissionsdbQueries.stop();
+            await productQueries.stop();
+            await shiftQueries.stop();
+            resolve();
+        });
     }
 }
