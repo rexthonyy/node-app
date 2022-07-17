c39521869cf0fb0c492d9cbb221225140a5cc2ab
rexthony <rexthonyy@gmail.com>
2022-07-17 19:48:10 +0100

    update

--- ShiftsSaleor/app.js
+++ ShiftsSaleor/app.js
@@ -4,16 +4,17 @@ const { graphqlUploadExpress } = require('graphql-upload');
 const express = require('express');
 const schema = require('./schema');
 
-const app = express();
-const server = new ApolloServer(schema);
-
-// This middleware should be added before calling `applyMiddleware`.
-app.use(graphqlUploadExpress());
-server.applyMiddleware({ app });
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-
-
-app.listen({ port: process.env.PORT }, () => {
-    console.log("SERVER LISTENING ON PORT " + process.env.PORT);
-});
+async function startServer() {
+    const server = new ApolloServer(schema);
+    await server.start();
+    const app = express();
+    // This middleware should be added before calling `applyMiddleware`.
+    app.use(graphqlUploadExpress());
+    server.applyMiddleware({ app });
+    app.use(express.static('public'));
+    //app.use(express.urlencoded({ extended: true }));
+    app.listen({ port: process.env.PORT }, () => {
+        console.log("SERVER LISTENING ON PORT " + process.env.PORT);
+    });
+}
+startServer();
