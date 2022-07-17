70ce20a1e3d7e22cf8e195e3c6ce37fccfa91289
rexthony <rexthonyy@gmail.com>
2022-07-17 15:29:41 +0100

    update

--- ShiftsSaleor/app.js
+++ ShiftsSaleor/app.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const { ApolloServer } = require("apollo-server-express");
+const { graphqlUploadExpress } = require("graphql-upload");
 const express = require('express');
 const schema = require('./schema');
 
 const app = express();
 const server = new ApolloServer(schema);
+
+app.use(graphqlUploadExpress());
 
 server.applyMiddleware({ app });
 
