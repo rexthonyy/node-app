622d30a3e4a7366df4c8bdc193b1519c6205696d
rexthony <rexthonyy@gmail.com>
2022-07-17 18:59:36 +0100

    update

--- ShiftsSaleor/app.js
+++ ShiftsSaleor/app.js
@@ -1,13 +1,20 @@
 require("dotenv").config();
 const { ApolloServer } = require("apollo-server-express");
+const {
+    GraphQLUpload,
+    graphqlUploadExpress, // A Koa implementation is also exported.
+} = require('graphql-upload');
 const express = require('express');
 const schema = require('./schema');
 
 const app = express();
 const server = new ApolloServer(schema);
 
+// This middleware should be added before calling `applyMiddleware`.
+app.use(graphqlUploadExpress());
 server.applyMiddleware({ app });
 app.use(express.static('public'));
+
 
 app.listen({ port: process.env.PORT }, () => {
     console.log("SERVER LISTENING ON PORT " + process.env.PORT);
