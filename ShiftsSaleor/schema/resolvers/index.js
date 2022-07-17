152cc11aaced036a83cb4e81f130961ea626faa1
rexthony <rexthonyy@gmail.com>
2022-07-17 19:28:11 +0100

    update

--- ShiftsSaleor/schema/resolvers/index.js
+++ ShiftsSaleor/schema/resolvers/index.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const fs = require('fs');
-const { finished } = require('stream/promises');
 const { GraphQLUpload } = require('graphql-upload');
 
 module.exports = {
@@ -17,7 +16,6 @@ module.exports = {
             const pathname = path.join(__dirname, `../../public/images/${filename}`);
             let out = fs.createWriteStream(pathname);
             await stream.pipe(out);
-            await finished(out);
 
             return {
                 url: `http://localhost:4000/images/${filename}`
