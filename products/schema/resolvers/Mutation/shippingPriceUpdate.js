51609e7d60db7497d885cd92efef37f77e65eb0f
rexthony <rexthonyy@gmail.com>
2022-07-21 18:01:22 +0100

    update

--- products/schema/resolvers/Mutation/shippingPriceUpdate.js
+++ products/schema/resolvers/Mutation/shippingPriceUpdate.js
@@ -138,14 +138,14 @@ function updateShippingMethod(args) {
                 return reject(err);
             }
         }
-        console.log(resolve); //.
+
         let { values, set, whereClause } = getShippingMethodUpdateInput(args);
         productQueries.updateShippingMethod(values, set, whereClause, result => {
             // if (result.err) return reject(getGraphQLOutput("updateShippingMethod", JSON.stringify(result.err), "GRAPHQL_ERROR").errors);
             // if (result.res.length == 0) return reject(getGraphQLOutput("updateShippingMethod", "Failed to update shipping method", "GRAPHQL_ERROR").errors);
-            //console.log(result.res);
-            //console.log(resolve);
-            //resolve();
+            console.log(result.res);
+            console.log(resolve);
+            resolve();
         });
     });
 }
