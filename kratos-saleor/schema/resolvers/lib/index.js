7f549133b12581ecc40565cc40df3ad4e3568d10
rexthony <rexthonyy@gmail.com>
2022-07-25 12:44:34 +0100

    update

--- kratos-saleor/schema/resolvers/lib/index.js
+++ kratos-saleor/schema/resolvers/lib/index.js
@@ -1,7 +1,9 @@
 const getGraphQLUserById = require('./getGraphQLUserById');
+const getGraphQLGiftCardById = require('./getGraphQLGiftCardById');
 const updateAccountUserDefaultAddressesByUserId = require('./updateAccountUserDefaultAddressesByUserId');
 
 module.exports = {
     getGraphQLUserById,
-    updateAccountUserDefaultAddressesByUserId
+    updateAccountUserDefaultAddressesByUserId,
+    getGraphQLGiftCardById,
 };
